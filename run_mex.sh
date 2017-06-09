cc84a0458967554b77fbea455d95fc6415293f14
Sam Potter <sfp@hush.ai>
2017-06-09 11:10:41 -0400

    handle release and debug builds in run_mex.sh

--- run_mex.sh
+++ run_mex.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 ROOT_DIR=$1
-BUILD_DIR=${ROOT_DIR}/build/release
+BUILD_DIR=${ROOT_DIR}/build/$2
 SRC_DIR=${ROOT_DIR}/src
 
 mex -outdir ${BUILD_DIR} -L${BUILD_DIR} ${SRC_DIR}/fmm.cpp -lfmm

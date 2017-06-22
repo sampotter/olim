cd315aae98380c8efe8797bd6fb1514c0445caff
Sam Potter <sfp@hush.ai>
2017-06-22 11:48:57 -0700

    added another speed func

--- src/speed_funcs.cpp
+++ src/speed_funcs.cpp
@@ -16,6 +16,22 @@ double sf1_soln(double x, double y) {
   return (a*a + y*y)/2;
 }
 
+double sf2(double x, double y) {
+  double x2 = x*x, y2 = y*y;
+  double denom = std::pow(x2 + y2, 4.5);
+  double sum = x + y;
+  double diff = x - y;
+  double x4 = x2*x2, y4 = y2*y2;
+  double vx = -4*x*y2*sum*diff*(x4 - 11*x2*y2 + 2*y4)/denom;
+  double vy = 4*x2*y*sum*diff*(2*x4 - 11*x2*y2 + y4)/denom;
+  return std::sqrt(vx*vx + vy*vy);
+}
+
+double sf2_soln(double x, double y) {
+  double x2 = x*x, y2 = y*y;
+  return 4*x2*y2*std::pow(x2 + y2, 2)/std::pow(x2 - y2, 3.5) + 1;
+}
+
 // Local Variables:
 // indent-tabs-mode: nil
 // c-basic-offset: 2
--- src/speed_funcs.hpp
+++ src/speed_funcs.hpp
@@ -4,6 +4,9 @@
 double sf1(double x, double y);
 double sf1_soln(double x, double y);
 
+double sf2(double x, double y);
+double sf2_soln(double x, double y);
+
 #endif // __SPEED_FUNCS_HPP__
 
 // Local Variables:

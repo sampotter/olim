efbe3e3d025269223fa4e47b52e6c907336a715d
Sam Potter <sfp@hush.ai>
2017-06-26 16:49:52 -0700

    replace sf1

--- src/speed_funcs.cpp
+++ src/speed_funcs.cpp
@@ -13,17 +13,12 @@ double default_speed_func_soln(double x, double y) {
 }
 
 double sf1(double x, double y) {
-  double a = (x + y)/2;
-  double b = std::cos(a);
-  double c = std::sin(b)/2;
-  double vx = (x + b)*(1 - c);
-  double vy = y - b*c;
-  return std::sqrt(vx*vx + vy*vy);
+  return 1 - std::sin(std::sqrt(x*x + y*y));
 }
 
 double sf1_soln(double x, double y) {
-  double a = x + std::cos((x + y)/2);
-  return (a*a + y*y)/2;
+  double r = std::sqrt(x*x + y*y);
+  return std::cos(r) + r - 1;
 }
 
 double sf2(double x, double y) {

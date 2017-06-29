19719eec6c77a948f882d344228bea08b84d1404
Sam Potter <sfp@hush.ai>
2017-06-29 12:07:54 -0700

    fix up fmm.cpp more

--- src/fmm.cpp
+++ src/fmm.cpp
@@ -1,20 +1,45 @@
 #include "mex.h"
 
-#include <cassert>
+#include <iostream>
 #include <string>
+#include <unordered_map>
 
 #include "fmm_mex.hpp"
 
-static mxArray * getDefaultPvaluesMatrix(int M, int N) {
-  mxArray * pvalues = mxCreateDoubleMatrix(M + 2, N + 2, mxREAL);
-  double * pv = mxGetPr(pvalues);
+/**
+ * This struct contains the parameters that need to be initialized in
+ * order to instantiate one of the C++ fast marcher classes.
+ */
+struct parameters {
+  marcher_type type;
+  mxArray * S;
+  double h;
+  double x0;
+  double y0;
+  int M;
+  int N;
+};
+
+/**
+ * The "S matrix" is a matrix of speed function values at each point
+ * in the computational domain, *plus* the boundary values (which are
+ * opaque to the user). This function fills this matrix with ones,
+ * corresponding to the "default" speed function.
+ */
+static mxArray * getDefaultSMatrix(int M, int N) {
+  mxArray * S = mxCreateDoubleMatrix(M + 2, N + 2, mxREAL);
+  double * Spr = mxGetPr(S);
   int nelts = (M + 2)*(N + 2);
   for (int i = 0; i < nelts; ++i) {
-    pv[i] = 1;
+    Spr[i] = 1;
   }
-  return pvalues;
+  return S;
 }
 
+/**
+ * Retrieve a double scalar value from an mxArray, issue an error is
+ * the mxArray isn't a double scalar.
+ */
 static double parseScalar(mxArray const * arg, const char * errMsgTxt) {
   if (!mxIsClass(arg, "double") || mxGetM(arg) > 1 || mxGetN(arg) > 1) {
     mexErrMsgTxt(errMsgTxt);
@@ -22,123 +47,120 @@ static double parseScalar(mxArray const * arg, const char * errMsgTxt) {
   return mxGetScalar(arg);
 }
 
-static void parseSpeedFunc(mxArray const * arg, mxArray *& pvalues, double h,
-                           double x0, double y0, int M, int N) {
+/**
+ * Retrieve a string from an mxArray, issue an error is the mxArray
+ * isn't a MATLAB string.
+ */
+static std::string parseString(mxArray const * arg, const char * errMsgTxt) {
+  if (!mxIsClass(arg, "char") || mxGetM(arg) != 1) {
+    mexErrMsgTxt(errMsgTxt);
+  }
+  return mxArrayToString(arg);
+}
+
+/**
+ * Fill the S matrix with the values of the user supplied speed
+ * function.
+ */
+static void parseSpeedFunc(mxArray const * arg, parameters & p) {
   if (!mxIsClass(arg, "function_handle")) {
     mexErrMsgTxt("Speed function argument must be a function.");
   }
 
-  mxArray * X = mxCreateDoubleMatrix(M + 2, N + 2, mxREAL);
-  mxArray * Y = mxCreateDoubleMatrix(M + 2, N + 2, mxREAL);
+  mxArray * X = mxCreateDoubleMatrix(p.M + 2, p.N + 2, mxREAL);
+  mxArray * Y = mxCreateDoubleMatrix(p.M + 2, p.N + 2, mxREAL);
 
-  double * Xpr = mxGetPr(X);
-  double * Ypr = mxGetPr(Y);
-
+  double * Xpr = mxGetPr(X), * Ypr = mxGetPr(Y);
   int k = 0;
-  for (int i = -1; i <= M; ++i) {
-    double y = h*i - y0;
-    for (int j = -1; j <= N; ++j) {
-      Xpr[k] = h*j - x0;
+  for (int i = -1; i <= p.M; ++i) {
+    double y = p.h*i - p.y0;
+    for (int j = -1; j <= p.N; ++j) {
+      Xpr[k] = p.h*j - p.x0;
       Ypr[k++] = y;
     }
   }
 
   mxArray * plhs[] = {nullptr};
   mxArray * prhs[] = {const_cast<mxArray *>(arg), X, Y};
-  mxArray * except = mexCallMATLABWithTrap(1, plhs, 3, prhs, "feval");
-  assert(except == nullptr);
-  mxDestroyArray(pvalues);
-  pvalues = plhs[0];
+  mxArray * e = mexCallMATLABWithTrap(1, plhs, 3, prhs, "feval");
+  if (e != nullptr) {
+    std::cout << mxArrayToString(mxGetProperty(e, 0, "identifier")) << ": "
+              << mxArrayToString(mxGetProperty(e, 0, "message")) << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
 
   mxDestroyArray(X);
   mxDestroyArray(Y);
+  mxDestroyArray(p.S); // free this before we lose our pointer to it
+
+  p.S = plhs[0];
 }
 
+/**
+ * Retrieve the marcher_type value from the MATLABstring passed as an
+ * argument.
+ */
 static marcher_type parseMarcherType(mxArray const * arg) {
-  if (!mxIsClass(arg, "char") || mxGetM(arg) != 1) {
-    mexErrMsgTxt("Argument containing method must be a string.");
-  }
-  std::string str {mxArrayToString(arg)};
+  auto const str = parseString(arg, "Method argument must be a string.");
   marcher_type type;
-  if (str == "basic") {
-    type = marcher_type::basic;
-  } else if (str == "olim8_mp0") {
-    type = marcher_type::olim8_mp0;
-  } else if (str == "olim8_mp1") {
-    type = marcher_type::olim8_mp1;
-  } else if (str == "olim8_rhr") {
-    type = marcher_type::olim8_rhr;
-  } else {
-    mexErrMsgTxt(("Invalid marcher type: " + str).c_str());
-  }
+  if (str == "basic") type = marcher_type::basic;
+  else if (str == "olim8_mp0") type = marcher_type::olim8_mp0;
+  else if (str == "olim8_mp1") type = marcher_type::olim8_mp1;
+  else if (str == "olim8_rhr") type = marcher_type::olim8_rhr;
+  else mexErrMsgTxt(("Invalid marcher type: " + str).c_str());
   return type;
 }
 
+/**
+ * Iterate through passed keyword arguments and fill the parameter
+ * struct.
+ */
 static void
 parseKeywordArguments(int nlhs, mxArray * plhs[],
                       int nrhs, mxArray const * prhs[],
-                      marcher_type & type, double & h, mxArray *& pvalues,
-                      int M, int N) {
+                      parameters & p) {
   if (nrhs % 2 == 0) {
     mexErrMsgTxt("Keyword arguments must be passed in pairs.");
   }
 
-  /*
-   * Need to parse h, x0, and y0 before parsing speed func keyword
-   */
-  double x0 = 0, y0 = 0;
+  std::unordered_map<std::string, mxArray const *> args;
   for (int i = 1; i < nrhs; i += 2) {
-    if (!mxIsClass(prhs[i], "char") || mxGetM(prhs[i]) != 1) {
-      mexErrMsgTxt("Keywords must be strings.");
-    }
-    std::string keyword {mxArrayToString(prhs[i])};
-    if (keyword == "h") {
-      h = parseScalar(
-        prhs[i + 1], "Keyword argument 'h' requires a scalar parameter.");
-    } else if (keyword == "x0") {
-      x0 = parseScalar(
-        prhs[i + 1], "Keyword argument 'x0' requires a scalar parameter.");
-    } else if (keyword == "y0") {
-      y0 = parseScalar(
-        prhs[i + 1], "Keyword argument 'y0' requires a scalar parameter.");
-    }
+    args[parseString(prhs[i], "Keywords must be strings.")] = prhs[i + 1];
   }
 
-  /*
-   * Parse the rest of the argument with h, x0, and y0 available
-   */
-  for (int i = 1; i < nrhs; i += 2) {
-    if (!mxIsClass(prhs[i], "char") || mxGetM(prhs[i]) != 1) {
-      mexErrMsgTxt("Keywords must be strings.");
-    }
-    std::string keyword {mxArrayToString(prhs[i])};
-    if (keyword == "h" || keyword == "x0" || keyword == "y0") {
-      continue;
-    }
-    if (keyword == "Speed") {
-      parseSpeedFunc(prhs[i + 1], pvalues, h, x0, y0, M, N);
-    } else if (keyword == "Method") {
-      type = parseMarcherType(prhs[i + 1]);
-    } else {
-      mexErrMsgTxt(("Invalid keyword: " + keyword).c_str());
-    }
-  }
+  if (args.find("Method") != args.end())
+    p.type = parseMarcherType(args["Method"]);
+
+  if (args.find("h") != args.end())
+    p.h = parseScalar(args["h"], "Keyword argument 'h' requires a scalar.");
+
+  if (args.find("x0") != args.end())
+    p.x0 = parseScalar(args["x0"], "Keyword argument 'x0' requires a scalar.");
+
+  if (args.find("y0") != args.end())
+    p.y0 = parseScalar(args["y0"], "Keyword argument 'y0' requires a scalar.");
+
+  if (args.find("Speed") != args.end())
+    parseSpeedFunc(args["Speed"], p);
 }
 
+/**
+ * Iterate through fixed position arguments and fill the parameter
+ * struct.
+ */
 static void
 parseRegularArguments(int nlhs, mxArray * plhs[],
                       int nrhs, mxArray const * prhs[],
-                      marcher_type & type, double & h, mxArray *& pvalues,
-                      int M, int N) {
+                      parameters & p) {
   if (nrhs >= 2) {
-    h = parseScalar(
+    p.h = parseScalar(
       prhs[1], "Second argument 'h' requires a scalar parameter.");
   }
   if (nrhs >= 3) {
-    parseSpeedFunc(prhs[2], pvalues, h, 0, 0, M, N);
+    parseSpeedFunc(prhs[2], p);
   }
   if (nrhs >= 4) {
-    type = parseMarcherType(prhs[3]);
+    p.type = parseMarcherType(prhs[3]);
   }
 }
 
@@ -167,14 +189,12 @@ void mexFunction(int nlhs, mxArray * plhs[], int nrhs, mxArray const * prhs[]) {
   /**
    * Set up arguments from passed parameters.
    */
-  marcher_type type = marcher_type::basic;
-  double h = 1;
-  mxArray * pvalues = getDefaultPvaluesMatrix(M, N);
-
+  parameters p {
+    marcher_type::basic, getDefaultSMatrix(M, N), 1, 0, 0, M, N};
   if (nrhs >= 2 && mxIsClass(prhs[1], "char") && mxGetM(prhs[1]) == 1) {
-    parseKeywordArguments(nlhs, plhs, nrhs, prhs, type, h, pvalues, M, N);
+    parseKeywordArguments(nlhs, plhs, nrhs, prhs, p);
   } else {
-    parseRegularArguments(nlhs, plhs, nrhs, prhs, type, h, pvalues, M, N);
+    parseRegularArguments(nlhs, plhs, nrhs, prhs, p);
   }
 
   /**
@@ -192,12 +212,13 @@ void mexFunction(int nlhs, mxArray * plhs[], int nrhs, mxArray const * prhs[]) {
   plhs[0] = mxCreateDoubleMatrix(M, N, mxREAL);
   double * out = mxGetPr(plhs[0]);
 
-  fmm_mex(out, in, M, N, h, mxGetPr(pvalues), type);
+  fmm_mex(out, in, M, N, p.h, mxGetPr(p.S), p.type);
 
   /**
    * Clean up.
    */
-  mxDestroyArray(pvalues);
+  free(in);
+  mxDestroyArray(p.S);
 }
 
 // Local Variables:

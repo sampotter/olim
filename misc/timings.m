932dee6649ae358460911ff8f495b9d42acd8f4d
Sam Potter <sfp@hush.ai>
2017-07-03 16:50:28 -0500

    update timings.m

--- misc/timings.m
+++ misc/timings.m
@@ -2,6 +2,8 @@ clear;
 
 path(path, '../build/Release');
 
+s1 = @(x, y) 1 - sin(sqrt(x.^2 + y.^2));
+
 ntrials = 10;
 mnum = 1;
 Ms = 2*ceil(logspace(1, 3, 10)/2) + 1;
@@ -14,10 +16,11 @@ for method = methods
     for M = Ms
         fprintf('- M = %d\n', M);
         B = zeros(M, 'logical');
-        B(1) = 1;
+        B((M + 1)/2, (M + 1)/2) = 1;
+        h = 2/(M - 1);
         for trial = 1:ntrials
             tic;
-            fmm(B, 'Method', method);
+            fmm(B, 'h', h, 'Method', method, 'Speed', s1, 'x0', 1, 'y0', 1);
             T(k, mnum) = min(T(k, mnum), toc);
         end
         k = k + 1;

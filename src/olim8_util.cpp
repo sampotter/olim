c6249eef8922a5c4aa0c2cf43cfc07bbbd310222
Sam Potter <sfp@hush.ai>
2017-07-05 12:25:09 -0400

    fix rhr updates

--- src/olim8_util.cpp
+++ src/olim8_util.cpp
@@ -19,32 +19,32 @@ static void check_params(double u0, double u1, double h) {
   assert(!std::isinf(u1));
 }
 
+/**
+ * Adjacent triangle update with constant quadrature rule (used by
+ * olim8_rhr and olim8_mp0).
+ */
 double rhr_adj(double u0, double u1, double s_est, double h) {
   check_params(u0, u1, h);
-  
-  double c = s_est*std::fabs(u0 - u1)/h;
-  if (c > std::sqrt(2)/2) {
-    return std::numeric_limits<double>::infinity();
-  }
-  double rad = sqrt(3)*c/sqrt(c*c + 2);
-  double lam1 = (1 + rad)/2;
-  double lam2 = (1 - rad)/2;
-  assert(std::fabs(1 - lam1 - lam2) < 1e-15 ||
-         (0 < lam1 && lam1 < 1) != (0 < lam2 && lam2 < 1));
-  double lam = 0 < lam1 && lam1 < 1 ? lam1 : lam2;
-  return (1 - lam)*u0 + lam*u1 + s_est*h*sqrt(2*lam*(1 - lam) + 1);
+  assert(s_est >= 0);
+
+  double c = (u0 - u1)/(s_est*h);
+  // TODO: use copysign for next line instead?
+  double lam = 0.5 + (c > 0 ? 1 : -1)*std::fabs(c)/(2*std::sqrt(2 - c*c));
+  return (1 - lam)*u0 + lam*u1 + s_est*h*sqrt(2*lam*(lam - 1) + 1);
 }
 
+/**
+ * Diagonal triangle update with constant quadrature rule (used by
+ * olim8_rhr and olim8_mp0).
+ */
 double rhr_diag(double u0, double u1, double s_est, double h) {
   check_params(u0, u1, h);
   assert(s_est >= 0);
   
-  double c = s_est*std::fabs(u0 - u1)/h;
-  if (c > std::sqrt(2)/2) {
-    return std::numeric_limits<double>::infinity();
-  }
-  double lam = c/std::sqrt(1 - c*c);
-  assert(0 <= lam && lam <= 1);
+  // TODO: make this one as simple as the adj rule
+  double c = std::fabs(u0 - u1)/(s_est*h);
+  double sgn = u0 > u1 ? 1 : -1;
+  double lam = std::max(0.0, std::min(1.0, sgn*c/std::sqrt(1 - c*c)));
   return (1 - lam)*u0 + lam*u1 + s_est*h*sqrt(lam*lam + 1);
 }
 

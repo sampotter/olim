6da8fe271a69e8bb182d7002b6ca9d75459e77ba
Sam Potter <sfp@hush.ai>
2017-07-07 16:43:51 -0400

    speed up basic marcher a bit

--- src/basic_marcher.cpp
+++ src/basic_marcher.cpp
@@ -8,15 +8,13 @@ void basic_marcher::update_node_value_impl(int i, int j, double & T) {
   node* nb[4] = {0x0, 0x0, 0x0, 0x0}; // NESW
   get_valid_neighbors(i, j, nb);
   double sh = get_h()*S(i, j);
-  double tmp = 0, T1 = 0, T2 = 0, disc = 0;
+  double T1 = 0, T2 = 0, disc = 0;
   for (int k = 0, k1 = 1; k < 4; ++k, k1 = (k1 + 1) % 4) {
     if (nb[k] && nb[k1]) {
       T1 = nb[k]->get_value();
       T2 = nb[k1]->get_value();
       disc = 2*sh*sh - (T1 - T2)*(T1 - T2);
-      if (disc <= 0) continue;
-      tmp = (T1 + T2 + std::sqrt(disc))/2;
-      if (tmp >= T1 && tmp >= T2) T = std::min(T, tmp);
+      T = disc > 0 ? std::min(T, (T1 + T2 + std::sqrt(disc))/2) : T;
     } else if (nb[k]) {
       T = std::min(T, nb[k]->get_value() + sh);
     }

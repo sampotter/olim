45e54a37f8d9d06c447671284eae81fe664eba61
Sam Potter <sfp@hush.ai>
2017-07-10 13:39:08 -0400

    try and optimize a little more (didn't really do anything)

--- src/olim4_rhr_lut.cpp
+++ src/olim4_rhr_lut.cpp
@@ -12,11 +12,10 @@ constexpr int NB_N = 8;
 void olim4_rhr_lut::update_node_value_impl(int i, int j, double & T) {
   node * nb[4] = {0x0, 0x0, 0x0, 0x0}; // NESW
   get_valid_neighbors(i, j, nb);
-  int const nb_lookup = (nb[0] ? NB_N : 0) | (nb[1] ? NB_E : 0) |
-    (nb[2] ? NB_S : 0) | (nb[3] ? NB_W : 0);
-  double s = S(i, j), h = get_h(), sh = s*h;
+  double s = S(i, j), h = get_h(), sh = s*h, tmp;
 
-  switch (nb_lookup) {
+  switch ((nb[0] ? NB_N : 0) | (nb[1] ? NB_E : 0) | (nb[2] ? NB_S : 0) |
+          (nb[3] ? NB_W : 0)) {
   /* one neighbor */
   case NB_N:
     T = nb[0]->get_value() + sh;
@@ -51,31 +50,36 @@ void olim4_rhr_lut::update_node_value_impl(int i, int j, double & T) {
     break;
   /* three neighbors */
   case NB_N | NB_E | NB_S:
+    tmp = nb[1]->get_value();
     T = std::min(
-      rhr_adj(nb[0]->get_value(), nb[1]->get_value(), s, h),
-      rhr_adj(nb[1]->get_value(), nb[2]->get_value(), s, h));
+      rhr_adj(nb[0]->get_value(), tmp, s, h),
+      rhr_adj(tmp, nb[2]->get_value(), s, h));
     break;
   case NB_N | NB_E | NB_W:
+    tmp = nb[0]->get_value();
     T = std::min(
-      rhr_adj(nb[0]->get_value(), nb[1]->get_value(), s, h),
-      rhr_adj(nb[3]->get_value(), nb[0]->get_value(), s, h));
+      rhr_adj(tmp, nb[1]->get_value(), s, h),
+      rhr_adj(nb[3]->get_value(), tmp, s, h));
     break;
   case NB_N | NB_S | NB_W:
+    tmp = nb[3]->get_value();
     T = std::min(
-      rhr_adj(nb[2]->get_value(), nb[3]->get_value(), s, h),
-      rhr_adj(nb[3]->get_value(), nb[0]->get_value(), s, h));
+      rhr_adj(nb[2]->get_value(), tmp, s, h),
+      rhr_adj(tmp, nb[0]->get_value(), s, h));
     break;
   case NB_E | NB_S | NB_W:
+    tmp = nb[2]->get_value();
     T = std::min(
-      rhr_adj(nb[1]->get_value(), nb[2]->get_value(), s, h),
-      rhr_adj(nb[2]->get_value(), nb[3]->get_value(), s, h));
+      rhr_adj(nb[1]->get_value(), tmp, s, h),
+      rhr_adj(tmp, nb[3]->get_value(), s, h));
     break;
   /* four neighbors */
   case NB_N | NB_E | NB_S | NB_W:
+    tmp = nb[1]->get_value();
     T = std::min(
       std::min(
-        rhr_adj(nb[0]->get_value(), nb[1]->get_value(), s, h),
-        rhr_adj(nb[1]->get_value(), nb[2]->get_value(), s, h)),
+        rhr_adj(nb[0]->get_value(), tmp, s, h),
+        rhr_adj(tmp, nb[2]->get_value(), s, h)),
       std::min(
         rhr_adj(nb[2]->get_value(), nb[3]->get_value(), s, h),
         rhr_adj(nb[3]->get_value(), nb[0]->get_value(), s, h)));

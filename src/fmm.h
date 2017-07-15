f7be573087d6f9dc0a907fb28678998b68757383
Sam Potter <sfp@hush.ai>
2017-07-14 20:10:13 -0400

    make things a little less MEX specific

---
+++ src/fmm.h
@@ -0,0 +1,15 @@
+#ifndef __FMM_H__
+#define __FMM_H__
+
+#include "typedefs.h"
+
+extern "C"
+void fmm(double * out, bool * in, int M, int N, double h, double * pvalues,
+         marcher_type type);
+
+#endif // __FMM_H__
+
+// Local Variables:
+// indent-tabs-mode: nil
+// c-basic-offset: 2
+// End:
---
+++ src/typedefs.h
@@ -0,0 +1,22 @@
+#ifndef __TYPEDEFS_H__
+#define __TYPEDEFS_H__
+
+enum marcher_type {
+  BASIC,
+  OLIM4_MP0,
+  OLIM4_RHR,
+  OLIM4_RHR_LUT,
+  OLIM8_MP0,
+  OLIM8_MP1,
+  OLIM8_RHR
+};
+
+typedef double(* speed_funcs)(double, double);
+typedef double(* speed_func_3d)(double, double, double);
+
+#endif // __TYPEDEFS_H__
+
+// Local Variables:
+// indent-tabs-mode: nil
+// c-basic-offset: 2
+// End:

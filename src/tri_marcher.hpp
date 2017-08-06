1a671a351559a1be946abb981f869c6d3f8687c2
Sam Potter <sfp@hush.ai>
2017-08-06 12:21:23 -0400

    tri_marcher stub

---
+++ src/tri_marcher.hpp
@@ -0,0 +1,35 @@
+#ifndef __TRI_MARCHER_HPP__
+#define __TRI_MARCHER_HPP__
+
+// TODO: for now, just to get the concept working and to start moving
+// forward with the smart marchers, we'll implement this for R2 (i.e.,
+// by specializing the node type to graph_node). Later, we'll make
+// this more general, and allow for an arbitrary Node type. This will
+// take a little more effort...
+
+#include <vector>
+
+#include "graph_node.hpp"
+
+struct tri_marcher;
+
+struct tri_neighbors {
+  using iterator = typename std::vector<graph_node *>::iterator;
+  void add_neighbor(graph_node * parent, graph_node * neighbor);
+  iterator begin();
+  iterator end();
+private:
+  std::vector<graph_node *> _neighbors;
+};
+
+struct tri_marcher: public graph_marcher<graph_node, tri_neighbors> {
+};
+
+#include "tri_marcher.impl.hpp"
+
+#endif // __TRI_MARCHER_HPP__
+
+// Local Variables:
+// indent-tabs-mode: nil
+// c-basic-offset: 2
+// End:
---
+++ src/tri_marcher.impl.hpp
@@ -0,0 +1,40 @@
+#ifndef __TRI_MARCHER_IMPL_HPP__
+#define __TRI_MARCHER_IMPL_HPP__
+
+#include <cmath>
+
+void tri_neighbors::add_neighbor(graph_node * parent, graph_node * neighbor) {
+  double const x0 = parent->get_x();
+  double const y0 = parent->get_y();
+  double const dx = neighbor->get_x() - x0;
+  double const dy = neighbor->get_y() - y0;
+  double const theta = atan2(dy, dx);
+
+  // Insert the new neighbor in decreasing order of angle (clockwise
+  // order, in keeping with previous implementations)
+  //
+  // TODO: for now, this just uses insertion sort, since the
+  // neighborhood size is likely to be very small. Later, it may be
+  // worth conditionally using a heavier duty algorithm here (i.e.,
+  // when the number of neighbors exceeds a threshold).
+  auto it = _neighbors.begin();
+  while (atan2(it->get_y() - y0, it->get_x() - x0) > theta) {
+    ++it;
+  }
+  _neighbors.insert(it, neighbor);
+}
+
+tri_neighbors::iterator begin() {
+  return _neighbors.begin();
+}
+
+tri_neighbors::iterator end() {
+  return _neighbors.end();
+}
+
+#endif // __TRI_MARCHER_IMPL_HPP__
+
+// Local Variables:
+// indent-tabs-mode: nil
+// c-basic-offset: 2
+// End:

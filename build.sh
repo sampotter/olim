82065f3afd96804b047329059de0d493f8669def
Sam Potter <sfp@hush.ai>
2017-08-15 09:44:02 -0400

    add simple build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ `uname -s` == "Linux" ]; then
+  rm -rf build
+  for build_type in Debug RelWithDebInfo Release
+  do
+    mkdir -p build/$build_type
+    cd build/$build_type
+    CXX=clang++ cmake -DCMAKE_BUILD_TYPE=$build_type -GNinja ../..
+    ninja -j `nproc`
+    cd -
+  done  
+elif [ `uname -s` == "Darwin" ]; then
+  echo "Darwin"
+else
+  echo "Unsupported platform" 1>&2
+  exit 1
+fi

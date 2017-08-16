859f0fe4d088e5bdc8294c5e4a432fb3ad8f6c59
Sam Potter <sfp@hush.ai>
2017-08-15 21:20:40 -0400

    build on osx

--- build.sh
+++ build.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 
 if [ `uname -s` == "Linux" ]; then
-  rm -rf build
-  for build_type in Debug RelWithDebInfo Release
-  do
-    mkdir -p build/$build_type
-    cd build/$build_type
-    CXX=clang++ cmake -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DCMAKE_BUILD_TYPE=$build_type -GNinja ../..
-    ninja -j `nproc`
-    cd -
-  done  
+	rm -rf build
+	for build_type in Debug RelWithDebInfo Release
+	do
+		mkdir -p build/$build_type
+		cd build/$build_type
+		CXX=clang++ cmake -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+		   -DCMAKE_BUILD_TYPE=$build_type -GNinja ../..
+		ninja -j `nproc`
+		cd -
+	done
 elif [ `uname -s` == "Darwin" ]; then
-  echo "Darwin"
+	rm -rf build
+	for build_type in Debug RelWithDebInfo Release
+	do
+		mkdir -p build/$build_type
+		cd build/$build_type
+		cmake -DCMAKE_BUILD_TYPE=$build_type -GNinja ../..
+		ninja -j `sysctl -n hw.ncpu`
+		cd -
+	done
 else
-  echo "Unsupported platform" 1>&2
-  exit 1
+	echo "Unsupported platform" 1>&2
+	exit 1
 fi
+
+# Local Variables:
+# indent-tabs-mode: nil
+# End:

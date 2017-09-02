e91215084f11ef306d6fbfc2804a844fed98ee06
Sam Potter <sfp@hush.ai>
2017-09-02 17:37:19 -0400

    forgot file-local-variables

--- src/olim6.defs.hpp
+++ src/olim6.defs.hpp
@@ -6,3 +6,8 @@ enum quadrant {NE, ES, SW, WN, UN, UE, US, UW, DN, DE, DS, DW};
 enum octant {UNE, UES, USW, UWN, DNE, DES, DSW, DWN};
 
 #endif // __OLIM6_DEFS_HPP__
+
+// Local Variables:
+// indent-tabs-mode: nil
+// c-basic-offset: 2
+// End:

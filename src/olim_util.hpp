3131c3af277ef0d6379a9df8dfa7c55c7bf5c389
Sam Potter <sfp@hush.ai>
2017-09-03 09:09:32 -0400

    oops; forgot headers

--- src/olim_util.hpp
+++ src/olim_util.hpp
@@ -1,9 +1,14 @@
 #ifndef __OLIM_UTIL_HPP__
 #define __OLIM_UTIL_HPP__
 
+template <int N>
+double rhr(double const * p0, double const * dp, double u0, double u1,
+           double s_est, double h);
+
 double rhr_adj(double u0, double u1, double s_est, double h,
                double * lam = nullptr);
 double rhr_diag(double u0, double u1, double s_est, double h);
+double rhr_3d22(double u0, double u1, double s_est, double h);
 
 #endif // __OLIM_UTIL_HPP__
 

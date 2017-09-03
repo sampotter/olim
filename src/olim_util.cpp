358a2823f5201600d2c4155878c9e8e237c5b10d
Sam Potter <sfp@hush.ai>
2017-09-03 09:08:17 -0400

    add "general rhr" for dims 2 and 3

--- src/olim_util.cpp
+++ src/olim_util.cpp
@@ -17,6 +17,79 @@ static void check_params(double u0, double u1, double h) {
   assert(!std::isinf(u1));
 }
 
+template <>
+double rhr<2>(double const * p0, double const * dp, double u0, double u1,
+              double s_est, double h) {
+  check_params(u0, u1, h);
+  assert(s_est >= 0);
+
+  double alpha = (u1 - u0)/s_est, alpha_sq = alpha*alpha,
+    p0normsq = p0[0]*p0[0] + p0[1]*p0[1],
+    d0dotdp = p0[0]*dp[0] + p0[1]*dp[1],
+    dpnormsq = dp[0]*dp[0] + dp[1]*dp[1],
+    a = (alpha_sq - 1)*p0normsq,
+    b = 2*p0dotdp*(alpha_sq - dpnormsq),
+    c = alpha_sq*p0normsq - p0dotdp*p0dotdp,
+    disc = b*b - 4*a*c;
+
+  assert(disc >= 0);
+
+  double lam = (-b + sqrt(disc))/(2*a),
+    plam[2] = {p0[0] + lam*dp[0], p0[1] + lam*dp[1]},
+    plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1]),
+    plamdotdp = plam[0]*dp[0] + plam[1]*dp[1];
+
+  if (fabs(alpha*plamnorm + plamdotdp) < 1e-13) {
+    return (1 - lam)*u0 + lam*u1 + s_est*plamnorm;
+  }
+
+  lam = -(b - sqrt(disc))/(2*a);
+  plam[0] = p0[0] + lam*dp[0];
+  plam[1] = p0[1] + lam*dp[1];
+  plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1]);
+  plamdotdp = plam[0]*dp[0] + plam[1]*dp[1];
+
+  assert(fabs(alpha*plamnorm + plamdotdp) < 1e-13);
+
+  return (1 - lam)*u0 + lam*u1 + s_est*plamnorm;
+}
+
+template <>
+double rhr<3>(double const * p0, double const * dp, double u0, double u1,
+              double s_est, double h) {
+  check_params(u0, u1, h);
+  assert(s_est >= 0);
+
+  double alpha = (u1 - u0)/s_est, alpha_sq = alpha*alpha,
+    p0normsq = p0[0]*p0[0] + p0[1]*p0[1] + p0[2]*p0[2],
+    p0dotdp = p0[0]*dp[0] + p0[1]*dp[1] + p0[2]*dp[2],
+    dpnormsq = dp[0]*dp[0] + dp[1]*dp[1] + dp[2]*dp[2],
+    a = (alpha_sq - 1)*p0normsq, b = 2*p0dotdp*(alpha_sq - dpnormsq),
+    c = alpha_sq*p0normsq - p0dotdp*p0dotdp, disc = b*b - 4*a*c;
+
+  assert(disc >= 0);
+
+  double lam = (-b + sqrt(disc))/(2*a),
+    plam[3] = {p0[0] + lam*dp[0], p0[1] + lam*dp[1], p0[2] + lam*dp[2]},
+    plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1] + plam[2]*plam[2]),
+    plamdotdp = plam[0]*dp[0] + plam[1]*dp[1] + plam[2]*dp[2];
+
+  if (fabs(alpha*plamnorm + plamdotdp) < 1e-13) {
+    return (1 - lam)*u0 + lam*u1 + s_est*plamnorm;
+  }
+
+  lam = -(b - sqrt(disc))/(2*a);
+  plam[0] = p0[0] + lam*dp[0];
+  plam[1] = p0[1] + lam*dp[1];
+  plam[2] = p0[2] + lam*dp[2];
+  plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1] + plam[2]*plam[2]);
+  plamdotdp = plam[0]*dp[0] + plam[1]*dp[1] + plam[2]*dp[2];
+
+  assert(fabs(alpha*plamnorm + plamdotdp) < 1e-13);
+
+  return (1 - lam)*u0 + lam*u1 + s_est*plamnorm;
+}
+
 /**
  * Adjacent triangle update with constant quadrature rule (used by
  * olim8_rhr and olim8_mp0c).

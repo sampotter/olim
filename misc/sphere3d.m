4d8ba8e465b1f8e16d434d8200487422c9ddf505
Sam Potter <sfp@hush.ai>
2017-09-04 15:36:59 -0400

    add sphere3d sanity check script

---
+++ misc/sphere3d.m
@@ -0,0 +1,33 @@
+% A simple script that plots a sphere using each of the 3D fast
+% marchers. This script is for use as a sanity check/debug script.
+
+clear;
+
+path(path, '../build/Release');
+
+M = 5;
+n = 2^M + 1;
+B = zeros(n, n, n, 'logical');
+B((n + 1)/2, (n + 1)/2, (n + 1)/2) = 1;
+h = 2/(n - 1);
+getU = @(method) fmm(B, 'Method', method, 'h', h, 'x0', 1, 'y0', 1, 'z0', 1);
+
+[x y z] = meshgrid(linspace(-1, 1, n), linspace(-1, 1, n), linspace(-1, 1, n));
+
+methods = {'basic', 'olim6_rhr_arma'};
+
+rows = 2;
+cols = 2;
+assert(length(methods) <= rows*cols);
+
+for k = 1:length(methods)
+    method = methods{k};
+    U = getU(method);
+    subplot(rows, cols, k);
+    title(strrep(method, '_', '\_'));
+    isosurface(x, y, z, U, 1);
+    colormap gray;
+    view(3);
+    camlight;
+    lighting flat;
+end

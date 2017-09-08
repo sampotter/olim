b57ef57ee56d87c652e10028c1f50ab75fd99a24
Sam Potter <sfp@hush.ai>
2017-09-08 13:40:04 -0400

    oops

--- runtest.cmake
+++ runtest.cmake
@@ -3,7 +3,7 @@ execute_process (
   OUTPUT_FILE tmp.txt
   RESULT_VARIABLE HAD_ERROR)
 
-if (HAD_ERROR)
+if (${HAD_ERROR})
   message (FATAL_ERROR "error running test")
 endif ()
 
@@ -12,6 +12,6 @@ execute_process(
   INPUT_FILE tmp.txt
   RESULT_VARIABLE TEST_FAILURE)
 
-if (TEST_FAILURE)
+if (${TEST_FAILURE})
   message (FATAL_ERROR "test failure")
 endif ()

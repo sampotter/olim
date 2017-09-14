ca10e19ff4f158aa6bfb790758b8446700878d5c
Sam Potter <sfp@hush.ai>
2017-09-14 11:22:21 -0400

    add olim18_rhr quadrant tests

--- src/olim18_rhr.test.cpp
+++ src/olim18_rhr.test.cpp
@@ -6,6 +6,10 @@
 void quadrants_are_correct() {
   int n = 2;
   double h = 1;
+
+  /*
+   * Tests for quadrants in the x-y plane:
+   */
   {
     olim18_rhr_arma m {n, n, 1, h, default_speed_func_3d, 0, 0, 0};
     m.add_boundary_node(0, 0, 0);
@@ -41,6 +45,86 @@ void quadrants_are_correct() {
     IS_APPROX_EQUAL(m.get_value(0, 1, 0), 1.0);
     IS_APPROX_EQUAL(m.get_value(1, 0, 0), 1.0);
     IS_APPROX_EQUAL(m.get_value(1, 1, 0), 0.0);
+  }
+
+  /**
+   * Tests for quadrants in the x-z plane:
+   */
+  {
+    olim18_rhr_arma m {n, 1, n, h, default_speed_func_3d, 0, 0, 0};
+    m.add_boundary_node(0, 0, 0);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), 0.0);
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), 1.0);
+    IS_APPROX_EQUAL(m.get_value(1, 0, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(1, 0, 1), sqrt(2));
+  }
+  {
+    olim18_rhr_arma m {n, 1, n, h, default_speed_func_3d, 0, 1, 0};
+    m.add_boundary_node(1, 0, 0);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), sqrt(2));
+    IS_APPROX_EQUAL(m.get_value(1, 0, 0), 0.0);
+    IS_APPROX_EQUAL(m.get_value(1, 0, 1), 1.0);
+  }
+  {
+    olim18_rhr_arma m {n, 1, n, h, default_speed_func_3d, 0, 0, 1};
+    m.add_boundary_node(0, 0, 1);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), 0.0);
+    IS_APPROX_EQUAL(m.get_value(1, 0, 0), sqrt(2));
+    IS_APPROX_EQUAL(m.get_value(1, 0, 1), 1.0);
+  }
+  {
+    olim18_rhr_arma m {n, 1, n, h, default_speed_func_3d, 0, 1, 1};
+    m.add_boundary_node(1, 0, 1);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), sqrt(2));
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), 1.0);
+    IS_APPROX_EQUAL(m.get_value(1, 0, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(1, 0, 1), 0.0);
+  }
+
+  /**
+   * Tests for quadrants in the y-z plane:
+   */
+  {
+    olim18_rhr_arma m {1, n, n, h, default_speed_func_3d, 0, 0, 0};
+    m.add_boundary_node(0, 0, 0);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), 0.0);
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 1, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 1, 1), sqrt(2));
+  }
+  {
+    olim18_rhr_arma m {1, n, n, h, default_speed_func_3d, 1, 0, 0};
+    m.add_boundary_node(0, 1, 0);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), sqrt(2));
+    IS_APPROX_EQUAL(m.get_value(0, 1, 0), 0.0);
+    IS_APPROX_EQUAL(m.get_value(0, 1, 1), 1.0);
+  }
+  {
+    olim18_rhr_arma m {1, n, n, h, default_speed_func_3d, 0, 0, 1};
+    m.add_boundary_node(0, 0, 1);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), 0.0);
+    IS_APPROX_EQUAL(m.get_value(0, 1, 0), sqrt(2));
+    IS_APPROX_EQUAL(m.get_value(0, 1, 1), 1.0);
+  }
+  {
+    olim18_rhr_arma m {1, n, n, h, default_speed_func_3d, 1, 0, 1};
+    m.add_boundary_node(0, 1, 1);
+    m.run();
+    IS_APPROX_EQUAL(m.get_value(0, 0, 0), sqrt(2));
+    IS_APPROX_EQUAL(m.get_value(0, 0, 1), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 1, 0), 1.0);
+    IS_APPROX_EQUAL(m.get_value(0, 1, 1), 0.0);
   }
 }
 

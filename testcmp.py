ebefd2f64e60b32748ad2160a160b1a686375844
Sam Potter <sfp@hush.ai>
2017-09-13 22:03:01 -0400

    add check for segfaults

--- testcmp.py
+++ testcmp.py
@@ -5,8 +5,11 @@ import fileinput
 def is_failure_line(line):
     return line[:7] == 'failure'
 
+def is_segfault_line(line):
+    return line[:18] == 'segmentation fault'
+
 def process_line(line):
-    if is_failure_line(line):
+    if is_failure_line(line) or is_sefault_line(line):
         sys.exit("test failure")
 
 if __name__ == '__main__':

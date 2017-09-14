1241aeb854a6e9f39593fa6ef3360fc9bb2487f9
Sam Potter <sfp@hush.ai>
2017-09-14 00:36:32 -0400

    this is easier to read

--- src/olim18.defs.hpp
+++ src/olim18.defs.hpp
@@ -4,7 +4,9 @@
 namespace olim18_defs {
   enum dir {
     N, E, U, S, W, D, // degree 1
+ // 0  1  2  3  4  5
     DS, DW, DE, UE, UN, DN, SW, SE, NE, NW, UW, US // degree 2
+ // 6   7   8   9   10  11  12  13  14  15  16  17
   };
 }
 

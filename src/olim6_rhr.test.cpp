ed2e0f691d52237bbe54d3bbc0bc1fba10b0cb81
Sam Potter <sfp@hush.ai>
2017-09-21 22:01:15 -0400

    add more olim6 tests (wip)

--- src/olim6_rhr.test.cpp
+++ src/olim6_rhr.test.cpp
@@ -1,5 +1,6 @@
 #include "test.hpp"
 
+#include "basic_marcher_3d.hpp"
 #include "olim4_rhr.hpp"
 #include "olim6_rhr.hpp"
 
@@ -221,12 +222,92 @@ void adj3pt_works() {
   IS_APPROX_EQUAL(val120, val201);
 }
 
+void two_by_two_by_three_cells_are_correct() {
+  int dims[3][3] = {{3, 2, 2}, {2, 3, 2}, {2, 2, 3}};
+
+  basic_marcher_3d m_gt {3, 2, 2, 1, default_speed_func_3d, 0, 0, 0};
+  m_gt.add_boundary_node(0, 0, 0);
+  m_gt.run();
+
+  for (int dim = 0, imax, jmax, kmax; dim < 3; ++dim) {
+    imax = dims[dim][0];
+    jmax = dims[dim][1];
+    kmax = dims[dim][2];
+
+    for (int corner = 0; corner < 8; ++corner) {
+      int i0 = (imax - 1)*(corner & 1);
+      int j0 = (jmax - 1)*((corner & 2) >> 1);
+      int k0 = (kmax - 1)*((corner & 4) >> 2);
+
+      double x0 = j0, y0 = i0, z0 = k0;
+      olim6_rhr_arma m {imax, jmax, kmax, 1, default_speed_func_3d, x0, y0, z0};
+      m.add_boundary_node(i0, j0, k0);
+      m.run();
+
+      int da = i0 == 0 ? 1 : -1;
+      int db = j0 == 0 ? 1 : -1;
+      int dc = k0 == 0 ? 1 : -1;
+      for (int i = 0, a = i0; i < imax; ++i, a += da) {
+        for (int j = 0, b = j0; j < jmax; ++j, b += db) {
+          for (int k = 0, c = k0, a_, b_, c_; k < kmax; ++k, c += dc) {
+            a_ = imax == 3 ? a : jmax == 3 ? b : c;
+            b_ = imax == 3 ? b : jmax == 3 ? c : a;
+            c_ = imax == 3 ? c : jmax == 3 ? a : b;
+            IS_APPROX_EQUAL(m.get_value(i, j, k), m_gt.get_value(a_, b_, c_));
+          }
+        }
+      }
+    }
+  }
+}
+
+void agrees_with_basic_marcher_3d() {
+  int n = 15;
+  double h = 2.0/(n - 1);
+
+  basic_marcher_3d m3d {n, n, n, h, default_speed_func_3d, 1, 1, 1};
+  m3d.add_boundary_node((n - 1)/2, (n - 1)/2, (n - 1)/2);
+  m3d.run();
+
+  olim6_rhr_arma m6 {n, n, n, h, default_speed_func_3d, 1, 1, 1};
+  m6.add_boundary_node((n - 1)/2, (n - 1)/2, (n - 1)/2);
+  m6.run();
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      for (int k = 0; k < n; ++k) {
+        IS_APPROX_EQUAL(m3d.get_value(i, j, k), m6.get_value(i, j, k));
+      }
+    }
+  }
+}
+
+void plane_boundaries_are_correct() {
+  int n = 2;
+  double h = 1;
+  olim6_rhr_arma m {n, n, n, h, default_speed_func_3d, 0, 0, 0};
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      m.add_boundary_node(i, j, 0);
+    }
+  }
+  m.run();
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      IS_APPROX_EQUAL(m.get_value(i, j, 1), 1.0);
+    }
+  }
+}
+
 int main() {
   quadrants_are_correct();
   octants_are_correct();
   planes_are_correct();
   result_is_symmetric();
-  // adj3pt_works();
+  adj3pt_works();
+  two_by_two_by_three_cells_are_correct();
+  agrees_with_basic_marcher_3d();
+  plane_boundaries_are_correct();
 }
 
 // Local Variables:

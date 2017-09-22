00b934c2960621115054010ea2a3cedb5fbcccce
Sam Potter <sfp@hush.ai>
2017-09-21 21:57:12 -0400

    rewrite tetra update for olim6

--- src/olim6_rhr.impl.hpp
+++ src/olim6_rhr.impl.hpp
@@ -30,25 +30,30 @@ double olim6_rhr_update_rules<rootfinder>::adj3pt(
 
   double sh = s*h, du1 = u1 - u0, du2 = u2 - u0;
   double alpha1 = fabs(du1/sh), alpha2 = fabs(du2/sh);
-  double alpha1_sq = alpha1*alpha1, alpha2_sq = alpha2*alpha2;
-  double A1 = 2 - alpha1_sq, B1 = 1 - 2*alpha1_sq, C1 = 1 - alpha1_sq;
-  double A2 = 2 - alpha2_sq, B2 = 1 - 2*alpha2_sq, C2 = 1 - alpha2_sq;
-  double AQ1[6] = {2*A1, 2*A1, B1, -2*A1, -2*C1, C1};
-  double AQ2[6] = {B2, 2*A2, 2*A2, -2*C2, -2*A1, C2};
+  double A = alpha1*alpha1, B = alpha2*alpha2;
+  double AQ1[6] = {2*A - 4, 2*A - 4, 2*A - 1, 4 - 2*A, 2 - 2*A, A - 1};
+  double AQ2[6] = {2*B - 1, 2*B - 4, 2*B - 4, 2 - 2*B, 4 - 2*B, B - 1};
 
   int n = 0;
   double isects[8];
+  for (int i = 0; i < 8; ++i) {
+    isects[i] = -1;
+  }
   this->intersect_conics(AQ1, AQ2, isects, n);
 
   double T = std::numeric_limits<double>::infinity();
-  double lam0, lam1, lam2, l;
-  for (int i = 0; i < n; i += 2) {
+  double lam0, lam1, lam2, l, lhs1, lhs2;
+  for (int i = 0; i < 2*n; i += 2) {
     lam1 = isects[i];
     lam2 = isects[i + 1];
+    if (lam1 < 0 || lam2 < 0 || lam1 + lam2 > 1) {
+      continue;
+    }
     lam0 = 1 - lam1 - lam2;
     l = sqrt(lam0*lam0 + lam1*lam1 + lam2*lam2);
-    if (fabs(2*lam1 + lam2 - 1 + du1*l/sh) < 1e-13 &&
-        fabs(2*lam1 + lam2 - 1 + du1*l/sh) < 1e-13) {
+    lhs1 = 2*lam1 + lam2 - 1 + du1*l/sh;
+    lhs2 = lam1 + 2*lam2 - 1 + du2*l/sh;
+    if (fabs(lhs1) < 1e-13 && fabs(lhs2) < 1e-13) {
       T = std::min(T, lam0*u0 + lam1*u1 + lam2*u2 + sh*l);
     }
   }

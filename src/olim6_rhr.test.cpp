dbc7de4a4d60e6e439422265b1876e1e05ae2015
Sam Potter <sfp@hush.ai>
2017-10-02 18:36:38 -0400

    add some more tests using nontriv speed funcs for olim6_rhr

--- src/olim6_rhr.test.cpp
+++ src/olim6_rhr.test.cpp
@@ -181,9 +181,63 @@ void planes_are_correct() {
   }
 }
 
+void planes_are_correct_for_nontrivial_speed_function() {
+  int n = 11;
+  double h = 1.0/(n/2);
+
+  olim4_rhr m4 {n, n, h, s1, 1, 1};
+  m4.add_boundary_node(n/2, n/2);
+  m4.run();
+
+  olim6_rhr m6 {n, n, n, h, s1, 1, 1, 1};
+  m6.add_boundary_node(n/2, n/2, n/2);
+  m6.run();
+
+  // Check that planes are correct:
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      double m4value = m4.get_value(i, j);
+      IS_APPROX_EQUAL(m4value, m6.get_value(i, j, n/2));
+      IS_APPROX_EQUAL(m4value, m6.get_value(i, n/2, j));
+      IS_APPROX_EQUAL(m4value, m6.get_value(n/2, i, j));
+    }
+  }
+}
+
 void result_is_symmetric() {
   int n = 5;
   olim6_rhr m {n, n, n, 1.0, default_speed_func_3d, 1.0, 1.0, 1.0};
+  m.add_boundary_node(n/2, n/2, n/2);
+  m.run();
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      for (int k = 0, k_ = n - 1; k < n; ++k, --k_) {
+        IS_APPROX_EQUAL(m.get_value(i, j, k), m.get_value(i, j, k_));
+      }
+    }
+  }
+
+  for (int i = 0; i < n; ++i) {
+    for (int k = 0; k < n; ++k) {
+      for (int j = 0, j_ = n - 1; j < n; ++j, --j_) {
+        IS_APPROX_EQUAL(m.get_value(i, j, k), m.get_value(i, j_, k));
+      }
+    }
+  }
+
+  for (int j = 0; j < n; ++j) {
+    for (int k = 0; k < n; ++k) {
+      for (int i = 0, i_ = n - 1; i < n; ++i, --i_) {
+        IS_APPROX_EQUAL(m.get_value(i, j, k), m.get_value(i_, j, k));
+      }
+    }
+  }
+}
+
+void result_is_symmetric_for_nontrivial_speed_function() {
+  int n = 5;
+  olim6_rhr m {n, n, n, 0.5, s1, 1.0, 1.0, 1.0};
   m.add_boundary_node(n/2, n/2, n/2);
   m.run();
 
@@ -316,7 +370,9 @@ int main() {
   quadrants_are_correct();
   octants_are_correct();
   planes_are_correct();
+  planes_are_correct_for_nontrivial_speed_function();
   result_is_symmetric();
+  result_is_symmetric_for_nontrivial_speed_function();
   two_by_two_by_three_cells_are_correct();
   agrees_with_basic_marcher_3d();
   agrees_with_basic_marcher_3d_for_nontrivial_speed_function();

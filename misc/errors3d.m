a07201cad3f7efa339c7157eb6c7f0e195985ba9
Sam Potter <sfp@hush.ai>
2017-10-02 12:18:51 -0400

    update errors3d

--- misc/errors3d.m
+++ misc/errors3d.m
@@ -5,6 +5,7 @@ path(path, '../build/Release');
 marks = {'o', '+', '*', '.', 'x', 's', 'd', '^', 'v', '<', '>', 'p', 'h'};
 
 ns = 2.^(2:9) + 1;
+% ns = 5:2:31;
 
 relerr = @(x, y, p) max(norm(x(:) - y(:), p)/norm(x(:), p), ...
                         norm(x(:) - y(:), p)/norm(y(:), p));
@@ -28,15 +29,24 @@ for n = ns
     % Compute solutions using different methods
     Ubasic = fmm(B, 'h', h, 'Method', 'basic', 'x0', 1, 'y0', 1, 'z0', 1);
     U6 = fmm(B, 'h', h, 'Method', 'olim6_rhr_arma', 'x0', 1, 'y0', 1, 'z0', 1);
+    U18 = fmm(B, 'h', h, 'Method', 'olim18_rhr_arma', 'x0', 1, 'y0', 1, 'z0', 1);
+    U26 = fmm(B, 'h', h, 'Method', 'olim26_rhr_arma', 'x0', 1, 'y0', 1, 'z0', 1);
 
+    % Compute relative errors
     Ebasic(k) = relerr(Ubasic, u, 'inf');
     E6(k) = relerr(U6, u, 'inf');
+    E18(k) = relerr(U18, u, 'inf');
+    E26(k) = relerr(U26, u, 'inf');
     
     k = k + 1;
 end
 
+% Plot errors
 figure;
 loglog(ns, Ebasic, '*');
 hold on;
 loglog(ns, E6, '+');
-legend('Basic', 'OLIM6 (right-hand rule)');
+loglog(ns, E18, 'o');
+loglog(ns, E26, 'x');
+legend('Basic', 'OLIM6 (right-hand rule)', 'OLIM18 (right-hand rule)', ...
+       'OLIM26 (right-hand rule)');

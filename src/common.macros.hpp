7c2b7bd2aaa670f0a171d68f58ff77837bc7fc9e
Sam Potter <sfp@hush.ai>
2017-10-02 21:19:00 -0400

    add EPS macro

--- src/common.macros.hpp
+++ src/common.macros.hpp
@@ -1,6 +1,7 @@
 #ifndef __COMMON_MACROS_HPP__
 #define __COMMON_MACROS_HPP__
 
+#define EPS(T) std::numeric_limits<T>::epsilon()
 #define VAL(i) (nb[i]->get_value())
 #define INF(T) std::numeric_limits<T>::infinity()
 

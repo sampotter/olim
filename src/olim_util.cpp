96c466787b956731fb146de7ad5a566fd32c0477
Sam Potter <sfp@hush.ai>
2017-10-03 00:21:50 -0400

    unused

--- src/olim_util.cpp
+++ src/olim_util.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <cassert>
 #include <cmath>
-#include <cstdio>
 #include <limits>
 
 #ifdef EIKONAL_DEBUG
@@ -29,81 +28,6 @@ void check_params(double u0, double u1, double u2, double h, double s) {
   assert(s >= 0);
 }
 #endif
-
-template <>
-double rhr<2>(double const * p0, double const * dp, double u0, double u1,
-              double s, double h) {
-#ifdef EIKONAL_DEBUG
-  check_params(u0, u1, h, s);
-#endif
-
-  double sh = s*h, alpha = (u1 - u0)/sh, alpha_sq = alpha*alpha,
-    p0normsq = p0[0]*p0[0] + p0[1]*p0[1],
-    p0dotdp = p0[0]*dp[0] + p0[1]*dp[1],
-    dpnormsq = dp[0]*dp[0] + dp[1]*dp[1],
-    a = (alpha_sq - 1)*p0normsq,
-    b = 2*p0dotdp*(alpha_sq - dpnormsq),
-    c = alpha_sq*p0normsq - p0dotdp*p0dotdp,
-    disc = b*b - 4*a*c;
-
-  assert(disc >= 0);
-
-  double lam = (-b + sqrt(disc))/(2*a),
-    plam[2] = {p0[0] + lam*dp[0], p0[1] + lam*dp[1]},
-    plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1]),
-    plamdotdp = plam[0]*dp[0] + plam[1]*dp[1];
-
-  if (fabs(alpha*plamnorm + plamdotdp) < 1e-13) {
-    return (1 - lam)*u0 + lam*u1 + s*plamnorm;
-  }
-
-  lam = -(b - sqrt(disc))/(2*a);
-  plam[0] = p0[0] + lam*dp[0];
-  plam[1] = p0[1] + lam*dp[1];
-  plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1]);
-  plamdotdp = plam[0]*dp[0] + plam[1]*dp[1];
-
-  assert(fabs(alpha*plamnorm + plamdotdp) < 1e-13);
-
-  return (1 - lam)*u0 + lam*u1 + s*plamnorm;
-}
-
-template <>
-double rhr<3>(double const * p0, double const * dp, double u0, double u1,
-              double s, double h) {
-#ifdef EIKONAL_DEBUG
-  check_params(u0, u1, h, s);
-#endif
-
-  double sh = s*h, alpha = (u1 - u0)/sh, alpha_sq = alpha*alpha,
-    p0normsq = p0[0]*p0[0] + p0[1]*p0[1] + p0[2]*p0[2],
-    p0dotdp = p0[0]*dp[0] + p0[1]*dp[1] + p0[2]*dp[2],
-    dpnormsq = dp[0]*dp[0] + dp[1]*dp[1] + dp[2]*dp[2],
-    a = (alpha_sq - 1)*p0normsq, b = 2*p0dotdp*(alpha_sq - dpnormsq),
-    c = alpha_sq*p0normsq - p0dotdp*p0dotdp, disc = b*b - 4*a*c;
-
-  assert(disc >= 0);
-
-  double lam = (-b + sqrt(disc))/(2*a),
-    plam[3] = {p0[0] + lam*dp[0], p0[1] + lam*dp[1], p0[2] + lam*dp[2]},
-    plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1] + plam[2]*plam[2]),
-    plamdotdp = plam[0]*dp[0] + plam[1]*dp[1] + plam[2]*dp[2];
-
-  if (fabs(alpha*plamnorm + plamdotdp) < 1e-13) {
-    return (1 - lam)*u0 + lam*u1 + s*plamnorm;
-  }
-
-  lam = -(b - sqrt(disc))/(2*a);
-  plam[0] = p0[0] + lam*dp[0];
-  plam[1] = p0[1] + lam*dp[1];
-  plam[2] = p0[2] + lam*dp[2];
-  plamnorm = sqrt(plam[0]*plam[0] + plam[1]*plam[1] + plam[2]*plam[2]);
-  plamdotdp = plam[0]*dp[0] + plam[1]*dp[1] + plam[2]*dp[2];
-
-  assert(fabs(alpha*plamnorm + plamdotdp) < 1e-13);
-
-  return (1 - lam)*u0 + lam*u1 + s*plamnorm;
-}
 
 /**
  * Adjacent triangle update with constant quadrature rule (used by
@@ -137,25 +61,6 @@ double rhr_diag(double u0, double u1, double s, double h) {
   double sgn = u0 > u1 ? 1 : -1;
   double lam = std::max(0.0, std::min(1.0, sgn*c/sqrt(1 - c*c)));
   return (1 - lam)*u0 + lam*u1 + sh*sqrt(lam*lam + 1);
-}
-
-double rhr_3d_22(double u0, double u1, double s, double h) {
-  using std::min;
-  using std::max;
-  using std::sqrt;
-
-#ifdef EIKONAL_DEBUG
-  check_params(u0, u1, h, s);
-#endif
-
-  double du = u1 - u0, sh = s*h, alpha = du/sh, alpha_sq = alpha*alpha;
-  double disc = 1 - 2*(2*alpha_sq - 1)/(alpha_sq - 2);
-
-  assert(disc >= 0);
-
-  double lam = max(0.0, min(1.0, (1 + (u0 > u1 ? 1 : -1)*sqrt(disc))/2));
-  double llam = sqrt(2*(lam*(lam - 1) + 1));
-  return (1 - lam)*u0 + lam*u1 + sh*llam;
 }
 
 // Local Variables:

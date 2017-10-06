4ccdead613faebbf29707cd07d5cd12084395ec4
Sam Potter <sfp@hush.ai>
2017-10-06 12:11:36 -0400

    add basic_marcher_3d to Python interface

--- src/py.eikonal.cpp
+++ src/py.eikonal.cpp
@@ -4,9 +4,13 @@
 #include "fmm.h"
 
 #include "basic_marcher.hpp"
+#include "basic_marcher_3d.hpp"
 
 namespace py = pybind11;
 using namespace py::literals;
+
+using speed_function = std::function<double(double, double)>;
+using speed_function_3d = std::function<double(double, double, double)>;
 
 PYBIND11_MODULE(eikonal, m) {
   m.doc() = "Testing testing";
@@ -25,7 +29,7 @@ PYBIND11_MODULE(eikonal, m) {
 
   py::class_<basic_marcher>(m, "BasicMarcher")
     .def(
-      py::init<int, int, double, std::function<double(double, double)>, double, double>(),
+      py::init<int, int, double, speed_function, double, double>(),
       "height"_a,
       "width"_a,
       "h"_a,
@@ -38,10 +42,27 @@ PYBIND11_MODULE(eikonal, m) {
       "i"_a,
       "j"_a,
       "value"_a = 0.0)
+    .def("getValue", &basic_marcher::get_value, "i"_a, "j"_a);
+
+  py::class_<basic_marcher_3d>(m, "BasicMarcher3D")
     .def(
-      "getValue", &basic_marcher::get_value,
+      py::init<int, int, int, double, speed_function_3d, double, double, double>(),
+      "height"_a,
+      "width"_a,
+      "depth"_a,
+      "h"_a,
+      "s"_a = py::cpp_function(default_speed_func_3d),
+      "x0"_a = 0.0,
+      "y0"_a = 0.0,
+      "z0"_a = 0.0)
+    .def("run", &basic_marcher_3d::run)
+    .def(
+      "addBoundaryNode", &basic_marcher_3d::add_boundary_node,
       "i"_a,
-      "j"_a);
+      "j"_a,
+      "k"_a,
+      "value"_a = 0.0)
+    .def("getValue", &basic_marcher_3d::get_value, "i"_a, "j"_a, "k"_a);
 }
 
 // Local Variables:

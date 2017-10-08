8cb65c54584430089fe54e49499a648b61aa833e
Sam Potter <sfp@hush.ai>
2017-10-08 13:58:04 -0400

    make build.sh and rebuild.sh a little nicer

--- build.sh
+++ build.sh
@@ -1,30 +1,42 @@
 #!/usr/bin/env bash
 
-if [ `uname -s` == "Linux" ]; then
-	rm -rf build
-	for build_type in Debug RelWithDebInfo Release
-	do
-		mkdir -p build/$build_type
-		cd build/$build_type
-		CXX=clang++ cmake -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
-		   -DCMAKE_BUILD_TYPE=$build_type -GNinja ../..
-		ninja -j `nproc`
-		cd -
-	done
-elif [ `uname -s` == "Darwin" ]; then
-	rm -rf build
-	for build_type in Debug RelWithDebInfo Release
-	do
-		mkdir -p build/$build_type
-		cd build/$build_type
-		cmake -DCMAKE_BUILD_TYPE=$build_type -GNinja ../..
-		ninja -j `sysctl -n hw.ncpu`
-		cd -
-	done
-else
-	echo "Unsupported platform" 1>&2
-	exit 1
-fi
+function error_exit {
+    echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+    exit 1
+}
+
+case `uname` in
+	'Darwin')
+		NPROC=`sysctl -n hw.ncpu`
+		;;
+	'Linux')
+		NPROC=`nproc`
+		;;
+	*)
+		error_exit "unsupported platform: `uname`"
+		;;
+esac
+
+function build_target {
+    CMAKE="cmake -DCMAKE_BUILD_TYPE=${1} -GNinja ../.."
+    case `uname` in
+        'Linux')
+            CMAKE='CXX=clang++ $CMAKE_CMD -DCMAKE_CXX_FLAGS="-stdlib=libc++"'
+            ;;
+        *)
+            ;;
+    esac
+    mkdir -p build/${1}
+    cd build/${1}
+    $CMAKE
+    ninja -j $NPROC
+    cd -
+}
+
+for BUILD_TYPE in Debug RelWithDebInfo Release
+do
+    build_target $BUILD_TYPE
+done
 
 # Local Variables:
 # indent-tabs-mode: nil

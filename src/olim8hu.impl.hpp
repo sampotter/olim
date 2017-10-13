a535eef9f0fc486a6ac08f0d04183e8af8b7d30b
Sam Potter <sfp@hush.ai>
2017-10-13 15:59:31 -0400

    fix this

--- src/olim8hu.impl.hpp
+++ src/olim8hu.impl.hpp
@@ -10,7 +10,7 @@ void olim8hu<update_rules>::update_impl(int i, int j, double & T) {
   abstract_node * x0 = 0x0;
   abstract_node * x1 = 0x0;
   get_valid_neighbors(i, j, nb);
-  double s = speed(i, j), h = get_h(), u0, Tnew;
+  double s = speed(i, j), h = get_h(), u0, s0, s1, Tnew;
 
   /**
    * Do every one-point update, and keep track of the index of the
@@ -20,7 +20,9 @@ void olim8hu<update_rules>::update_impl(int i, int j, double & T) {
   for (int k = 0; k < 8; ++k) {
     if ((x0 = nb[k])) {
       u0 = x0->get_value();
-      Tnew = k % 2 == 0 ? this->adj1pt(u0, s, h) : this->diag1pt(u0, s, h);
+      s0 = speed(i + di[k], j + dj[k]);
+      Tnew = k % 2 == 0 ? this->adj1pt(u0, s, s0, h) :
+        this->diag1pt(u0, s, s0, h);
       if (Tnew < T) {
         T = Tnew;
         argmin = k;
@@ -35,24 +37,32 @@ void olim8hu<update_rules>::update_impl(int i, int j, double & T) {
    */
   int kprev = (argmin + 7) % 8, knext = (argmin + 1) % 8;
   u0 = nb[argmin]->get_value();
+  s0 = speed(i + di[argmin], j + dj[argmin]);
   if (argmin % 2 == 0) {
     if ((x1 = nb[kprev])) {
-      T = std::min(T, this->diag2pt(u0, x1->get_value(), s, h));
+      s1 = speed(i + di[kprev], j + dj[kprev]);
+      T = std::min(T, this->diag2pt(u0, x1->get_value(), s, s0, s1, h));
     }
     if ((x1 = nb[knext])) {
-      T = std::min(T, this->diag2pt(u0, x1->get_value(), s, h));
+      s1 = speed(i + di[knext], j + dj[knext]);
+      T = std::min(T, this->diag2pt(u0, x1->get_value(), s, s0, s1, h));
     }
   } else {
     x1 = nb[kprev];
     abstract_node * x2 = nb[knext];
     if (x1) {
-      T = std::min(T, this->diag2pt(x1->get_value(), u0, s, h));
+      s1 = speed(i + di[kprev], j + dj[kprev]);
+      T = std::min(T, this->diag2pt(x1->get_value(), u0, s, s1, s0, h));
     }
     if (x2) {
-      T = std::min(T, this->diag2pt(x2->get_value(), u0, s, h));
+      s1 = speed(i + di[knext], j + dj[knext]);
+      T = std::min(T, this->diag2pt(x2->get_value(), u0, s, s1, s0, h));
     }
     if (x1 && x2) {
-      T = std::min(T, this->adj2pt(x1->get_value(), x2->get_value(), s, h));
+      s0 = speed(i + di[kprev], j + dj[kprev]);
+      s1 = speed(i + di[knext], j + dj[knext]);
+      T = std::min(T, this->adj2pt(x1->get_value(), x2->get_value(),
+                                   s, s0, s1, h));
     }
   }
 }

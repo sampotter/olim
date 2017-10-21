831d1d5f0bf0f71863c96322d0946c1a41893426
Sam Potter <sfp@hush.ai>
2017-10-21 14:38:15 -0400

    important comment for clarification

--- src/update_rules.line_updates.hpp
+++ src/update_rules.line_updates.hpp
@@ -3,9 +3,22 @@
 
 namespace update_rules {
   struct line_updates {
+    /**
+     * This line update is for use with "rectangular" OLIMs (RHR and
+     * MP0).
+     */
     template <int N>
     double line(double u0, double s, double h) const;
     
+    /**
+     * This line update is for the MP1 OLIMs.
+     *
+     * TODO: the name clash between this line update function and the
+     * preceding function is probably not a big deal, but if we ever
+     * start looking at methods which use a higher-order line update,
+     * then we'll need to come up with a better naming or template
+     * specialization convention.
+     */
     template <int N>
     double line(double u0, double s, double s0, double h) const;
   };

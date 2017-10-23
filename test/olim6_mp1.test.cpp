271077bec6c9b5f4b6459e6e4017dec58f6b59b2
Sam Potter <sfp@hush.ai>
2017-10-23 11:37:09 -0400

    add start of olim6_mp1 tests

---
+++ test/olim6_mp1.test.cpp
@@ -0,0 +1,37 @@
+#include "test.hpp"
+
+#include "basic_marcher_3d.hpp"
+#include "olim4.hpp"
+#include "olim6.hpp"
+
+void planes_are_correct() {
+  int n = 3;
+  double h = 1.0/(n/2);
+
+  olim4_mp1 m4 {n, n, h, default_speed_func, 1, 1};
+  m4.add_boundary_node(n/2, n/2);
+  m4.run();
+
+  olim6_mp1 m6 {n, n, n, h, default_speed_func_3d, 1, 1, 1};
+  m6.add_boundary_node(n/2, n/2, n/2);
+  m6.run();
+
+  // Check that planes are correct:
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      double m4value = m4.get_value(i, j);
+      IS_APPROX_EQUAL(m4value, m6.get_value(i, j, n/2));
+      IS_APPROX_EQUAL(m4value, m6.get_value(i, n/2, j));
+      IS_APPROX_EQUAL(m4value, m6.get_value(n/2, i, j));
+    }
+  }
+}
+
+int main() {
+  planes_are_correct();
+}
+
+// Local Variables:
+// indent-tabs-mode: nil
+// c-basic-offset: 2
+// End:

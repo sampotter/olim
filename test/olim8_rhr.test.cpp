f903eebabb67cd17216dcb680eaa3ae32a84ee47
Sam Potter <sfp@hush.ai>
2017-11-05 15:39:38 -0500

    fix olim8_rhr tests

--- test/olim8_rhr.test.cpp
+++ test/olim8_rhr.test.cpp
@@ -42,39 +42,6 @@ void neighboring_values_are_correct() {
   }
 }
 
-void slightly_more_involved() {
-  olim8_rhr m {2, 2, 1};
-  m.add_boundary_node(0, 0);
-  m.add_boundary_node(1, 0);
-  m.run();
-  IS_APPROX_EQUAL(m.get_value(0, 0), 0.0);
-  IS_APPROX_EQUAL(m.get_value(1, 0), 0.0);
-  IS_APPROX_EQUAL(m.get_value(0, 1), 1.0);
-  IS_APPROX_EQUAL(m.get_value(1, 1), 1.0);
-}
-
-void slightly_more_involved_2() {
-  olim8_rhr m {2, 2, 1};
-  m.add_boundary_node(0, 0);
-  m.run();
-  IS_APPROX_EQUAL(m.get_value(0, 0), 0.0);
-  IS_APPROX_EQUAL(m.get_value(1, 0), 1.0);
-  IS_APPROX_EQUAL(m.get_value(0, 1), 1.0);
-}
-
-void run_on_full_neighborhood() {
-  olim8_rhr m {3, 3, 1};
-  m.add_boundary_node(0, 0);
-  m.add_boundary_node(0, 1);
-  m.add_boundary_node(0, 2);
-  m.add_boundary_node(1, 0);
-  m.add_boundary_node(1, 2);
-  m.add_boundary_node(2, 0);
-  m.add_boundary_node(2, 1);
-  m.add_boundary_node(2, 2);
-  m.run();
-}
-
 void maria_test() {
   olim8_rhr m {3, 2, 1};
   m.add_boundary_node(0, 0);
@@ -101,71 +68,34 @@ void origin_test() {
   }
 }
 
-void s1_single_row_test() {
-  int N = 1001;
-  double h = 1.0/(N - 1);
-  olim8_rhr m {1, N, h, (speed_func) s1};
-  m.add_boundary_node(0, 0);
+void slightly_more_involved() {
+  olim8_rhr m {2, 2, 1};
+  node nodes[2] = {{0, 0}, {1, 0}};
+  m.add_boundary_nodes(nodes, 2);
   m.run();
-  double maxerr = 0;
-  for (int j = 0; j < N; ++j) {
-    double U = m.get_value(0, j);
-    double u = f1(h*j, 0);
-    maxerr = std::max(maxerr, std::fabs(U - u)/std::fabs(u));
-  }
-  std::cout << maxerr << std::endl;
+  IS_APPROX_EQUAL(m.get_value(0, 0), 0.0);
+  IS_APPROX_EQUAL(m.get_value(1, 0), 0.0);
+  IS_APPROX_EQUAL(m.get_value(0, 1), 1.0);
+  IS_APPROX_EQUAL(m.get_value(1, 1), 1.0);
 }
 
-void s1_test1() {
-  int M = 1001, N = M;
-  double h = 1.0/(M - 1);
-  olim8_rhr m {M, N, h, (speed_func) s1};
+void slightly_more_involved_2() {
+  olim8_rhr m {2, 2, 1};
   m.add_boundary_node(0, 0);
   m.run();
-  double maxerr = 0;
-  for (int i = 0; i < M; ++i) {
-    for (int j = 0; j < N; ++j) {
-      double U = m.get_value(i, j);
-      double u = f1(h*j, h*i);
-      if (u != 0) {
-        maxerr = std::max(maxerr, std::fabs(U - u)/std::fabs(u));
-      }
-    }
-  }
-  std::cout << maxerr << std::endl;
-}
-
-void s1_test2() {
-  int M = 201, N = M;
-  double h = 2.0/(M - 1);
-  olim8_rhr m {M, N, h, (speed_func) s1};
-  m.add_boundary_node(100, 100);
-  m.run();
-  double maxerr = 0;
-  for (int i = 0; i < M; ++i) {
-    for (int j = 0; j < N; ++j) {
-      double U = m.get_value(i, j);
-      double u = f1(h*j, h*i);
-      if (u != 0) {
-        maxerr = std::max(maxerr, std::fabs(U - u)/std::fabs(u));
-      }
-    }
-  }
-  std::cout << maxerr << std::endl;
+  IS_APPROX_EQUAL(m.get_value(0, 0), 0.0);
+  IS_APPROX_EQUAL(m.get_value(1, 0), 1.0);
+  IS_APPROX_EQUAL(m.get_value(0, 1), 1.0);
 }
 
 int main() {
   trivial_case_works();
   adjacent_update_works();
   neighboring_values_are_correct();
-  slightly_more_involved();
-  slightly_more_involved_2();
-  run_on_full_neighborhood();
   maria_test();
   origin_test();
-  s1_single_row_test();
-  s1_test1();
-  s1_test2();
+  slightly_more_involved();
+  slightly_more_involved_2();
 }
 
 // Local Variables:

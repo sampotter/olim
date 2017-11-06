df0777ac19ce462cdd812b9d8387f7f23991a5e2
Sam Potter <sfp@hush.ai>
2017-11-05 21:35:48 -0500

    templatize two_by_two_by_three cells are correct

--- test/olim26_rhr.test.cpp
+++ test/olim26_rhr.test.cpp
@@ -4,45 +4,6 @@
 #include "olim8.hpp"
 #include "olim26.hpp"
 
-void two_by_two_by_three_cells_are_correct() {
-  int dims[3][3] = {{3, 2, 2}, {2, 3, 2}, {2, 2, 3}};
-
-  olim26_rhr m_gt {3, 2, 2, 1, default_speed_func_3d, 0, 0, 0};
-  m_gt.add_boundary_node(0, 0, 0);
-  m_gt.run();
-
-  for (int dim = 0, imax, jmax, kmax; dim < 3; ++dim) {
-    imax = dims[dim][0];
-    jmax = dims[dim][1];
-    kmax = dims[dim][2];
-
-    for (int corner = 0; corner < 8; ++corner) {
-      int i0 = (imax - 1)*(corner & 1);
-      int j0 = (jmax - 1)*((corner & 2) >> 1);
-      int k0 = (kmax - 1)*((corner & 4) >> 2);
-
-      double x0 = j0, y0 = i0, z0 = k0;
-      olim26_rhr m {imax, jmax, kmax, 1, default_speed_func_3d, x0, y0, z0};
-      m.add_boundary_node(i0, j0, k0);
-      m.run();
-
-      int da = i0 == 0 ? 1 : -1;
-      int db = j0 == 0 ? 1 : -1;
-      int dc = k0 == 0 ? 1 : -1;
-      for (int i = 0, a = i0; i < imax; ++i, a += da) {
-        for (int j = 0, b = j0; j < jmax; ++j, b += db) {
-          for (int k = 0, c = k0, a_, b_, c_; k < kmax; ++k, c += dc) {
-            a_ = imax == 3 ? a : jmax == 3 ? b : c;
-            b_ = imax == 3 ? b : jmax == 3 ? c : a;
-            c_ = imax == 3 ? c : jmax == 3 ? a : b;
-            IS_APPROX_EQUAL(m.get_value(i, j, k), m_gt.get_value(a_, b_, c_));
-          }
-        }
-      }
-    }
-  }
-}
-
 void plane_boundaries_are_correct() {
   int n = 2;
   double h = 1;
@@ -71,7 +32,7 @@ int main() {
   quadrants_are_correct<olim3d>(sqrt(2));
   octants_are_correct<olim3d>(sqrt(2), sqrt(3));
   planes_are_correct<olim, olim3d>();
-  two_by_two_by_three_cells_are_correct();
   result_is_symmetric<olim3d>();
+  two_by_two_by_three_cells_are_correct<olim3d>();
   plane_boundaries_are_correct();
 }
--- test/olim6_mp0.test.cpp
+++ test/olim6_mp0.test.cpp
@@ -5,29 +5,6 @@
 #include "olim4.hpp"
 #include "olim6.hpp"
 
-void planes_are_correct() {
-  int n = 11;
-  double h = 1.0/(n/2);
-  
-  olim4_mp0 m4 {n, n, h, default_speed_func, 1, 1};
-  m4.add_boundary_node(n/2, n/2);
-  m4.run();
-  
-  olim6_mp0 m6 {n, n, n, h, default_speed_func_3d, 1, 1, 1};
-  m6.add_boundary_node(n/2, n/2, n/2);
-  m6.run();
-
-  // Check that planes are correct:
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      double m4value = m4.get_value(i, j);
-      IS_APPROX_EQUAL(m4value, m6.get_value(i, j, n/2));
-      IS_APPROX_EQUAL(m4value, m6.get_value(i, n/2, j));
-      IS_APPROX_EQUAL(m4value, m6.get_value(n/2, i, j));
-    }
-  }
-}
-
 int main() {
   using olim = olim4_mp0;
   using olim3d = olim6_mp0;
@@ -35,5 +12,6 @@ int main() {
   quadrants_are_correct<olim3d>(1 + sqrt(2)/2);
   octants_are_correct<olim3d>(1.0 + sqrt(2)/2, 1.0 + sqrt(2)/2 + sqrt(3)/3);
   planes_are_correct<olim, olim3d>();
+  two_by_two_by_three_cells_are_correct<olim3d>();
   result_is_symmetric<olim3d>();
 }

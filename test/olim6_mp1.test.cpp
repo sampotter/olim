08a701445c8c67803bc986d8ddb5b9daa1905441
Sam Potter <sfp@hush.ai>
2017-11-06 11:27:42 -0500

    switch order

--- test/olim6_mp1.test.cpp
+++ test/olim6_mp1.test.cpp
@@ -12,7 +12,7 @@ int main() {
   quadrants_are_correct<olim3d>(1 + sqrt(2)/2);
   octants_are_correct<olim3d>(1.0 + sqrt(2)/2, 1.0 + sqrt(2)/2 + sqrt(3)/3);
   planes_are_correct<olim, olim3d>();
-  result_is_symmetric<olim3d>();
   two_by_two_by_three_cells_are_correct<olim3d>();
+  result_is_symmetric<olim3d>();
   plane_boundaries_are_correct<olim3d>();
 }

ef71fcc012bd989b2cb6e8bf2e4557aff4fb20d2
Sam Potter <sfp@hush.ai>
2017-11-07 12:49:27 -0500

    tinkering

--- misc/newton222mp1.m
+++ misc/newton222mp1.m
@@ -9,9 +9,15 @@ function newton222mp1(u0, u1, u2, s0, s1, s2, h)
         u2 = 0;
     end
     if nargin < 4
-        s = 1;
+        s0 = 1;
     end
     if nargin < 5
+        s1 = 1;
+    end
+    if nargin < 6
+        s2 = 1;
+    end
+    if nargin < 7
         h = 1;
     end
 
@@ -33,14 +39,31 @@ function newton222mp1(u0, u1, u2, s0, s1, s2, h)
 
     X(1, :) = [1/3; 1/3];
     
-    niter = 50;
+    F_ = @(x, y) (1 - x - y)*u0 + x*u1 + y*u2 + h*s(x, y)*l(x, y);
+    Fvals(1) = F_(X(1, 1), X(1, 2));
+
+    niter = 200;
+    step = 1;
+    scale = 0.9;
+    stepsizes(1) = 1;
     for k = (1:niter) + 1
         x = X(k - 1, 1);
         y = X(k - 1, 2);
         p = H(x, y)\g(x, y);
-        X(k, :) = [x; y] - p;
+
+        % Backtracking line search
+        tmp = [x; y] - step*p;
+        while F_(tmp(1), tmp(2)) > Fvals(k - 1)
+            step = scale*step;
+            tmp = [x; y] - step*p;
+        end
+        stepsizes(k) = step;
+        X(k, :) = tmp;
+
+        Fvals(k) = F_(X(k, 1), X(k, 2));
         E(k - 1) = norm(p, 'inf')/norm(X(k, :), 'inf');
-        if E(k - 1) < 1e-15
+        if E(k - 1) < eps || abs(Fvals(k) - Fvals(k - 1)) < eps ...
+                || any(X(k, :) < 0) || sum(X(k, :)) > 1
             break;
         end
     end
@@ -55,7 +78,8 @@ function newton222mp1(u0, u1, u2, s0, s1, s2, h)
     F = (1 - x - y)*u0 + x*u1 + y*u2 + h*s(x, y).*l(x, y);
 
     figure;
-    subplot(1, 2, 1);
+
+    subplot(2, 2, 1);
     hold on;
     contour(x, y, F, 15);
     plot([0 1 0 0], [0 0 1 0], 'k');
@@ -63,7 +87,16 @@ function newton222mp1(u0, u1, u2, s0, s1, s2, h)
     xlim([min_, max_]);
     ylim([min_, max_]);
     scatter(X(size(X, 1), 1), X(size(X, 1), 2), 100);
-    subplot(1, 2, 2);
+
+    subplot(2, 2, 2);
     semilogy(1:length(E), E, '-x');
     xlim([0, length(E) + 1]);
+
+    subplot(2, 2, 3);
+    semilogy(1:length(Fvals), abs(Fvals - Fvals(length(Fvals))));
+    xlim([1 length(Fvals)]);
+
+    subplot(2, 2, 4);
+    plot((1:length(stepsizes)) + 1, stepsizes);
+
 end

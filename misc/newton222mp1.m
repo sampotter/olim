c4f986bd5cd487f47da8f02af44d4e72916f2a6d
Sam Potter <sfp@hush.ai>
2017-11-10 23:00:18 -0500

    use sbar

--- misc/newton222mp1.m
+++ misc/newton222mp1.m
@@ -24,10 +24,14 @@ function T = newton222mp1(u0, u1, u2, s, s0, s1, s2, h)
         h = 1;
     end
 
+    sbar0 = (s + s0)/2;
+    sbar1 = (s + s1)/2;
+    sbar2 = (s + s2)/2;
+    dsbar = [sbar1; sbar2] - sbar0;
+
     du = [u1; u2] - u0;
-    ds = [s1; s2] - s0;
     
-    s = @(x, y) (1 - x - y)*s0 + x*s1 + y*s2;
+    sbar = @(x, y) (1 - x - y)*sbar0 + x*sbar1 + y*sbar2;
 
     M = [2 1; 1 2];
     e = [-1; -1];
@@ -36,13 +40,13 @@ function T = newton222mp1(u0, u1, u2, s, s0, s1, s2, h)
     Q = @(x, y) [x y 1]*[M e; e' f]*[x; y; 1];
     l = @(x, y) sqrt((1 - x).^2 + (1 - y).^2 + (x + y).^2);
     
-    g = @(x, y) du + h*(l(x, y)*ds + (s(x, y)/l(x, y))*(M*[x; y] + e));
-    H = @(x, y) h*(2*(ds - (s(x, y)/(2*Q(x, y)))*(M*[x; y] + e))*(M*[x; ...
-                        y] + e)' + s(x, y)*M)/l(x, y);
+    g = @(x, y) du + h*(l(x, y)*dsbar + (sbar(x, y)/l(x, y))*(M*[x; y] + e));
+    H = @(x, y) h*(2*(dsbar - (sbar(x, y)/(2*Q(x, y)))*(M*[x; y] + e))*(M*[x; ...
+                        y] + e)' + sbar(x, y)*M)/l(x, y);
 
     X(1, :) = [1/3; 1/3];
     
-    F_ = @(x, y) (1 - x - y)*u0 + x*u1 + y*u2 + h*s(x, y)*l(x, y);
+    F_ = @(x, y) (1 - x - y)*u0 + x*u1 + y*u2 + h*sbar(x, y)*l(x, y);
     Fvals(1) = F_(X(1, 1), X(1, 2));
 
     niter = 200;
@@ -83,7 +87,7 @@ function T = newton222mp1(u0, u1, u2, s, s0, s1, s2, h)
     max_ = 2;
     lin = linspace(min_, max_, 121);
     [x y] = meshgrid(lin, lin);
-    F = (1 - x - y)*u0 + x*u1 + y*u2 + h*s(x, y).*l(x, y);
+    F = (1 - x - y)*u0 + x*u1 + y*u2 + h*sbar(x, y).*l(x, y);
 
     figure;
 

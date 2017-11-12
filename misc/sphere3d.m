89f82024cd4539973e6a74813963e0250e86505f
Sam Potter <sfp@hush.ai>
2017-11-12 17:05:05 -0500

    other method types

--- misc/sphere3d.m
+++ misc/sphere3d.m
@@ -14,7 +14,9 @@ getU = @(method) fmm(B, 'Method', method, 'h', h, 'x0', 1, 'y0', 1, 'z0', 1);
 
 [x y z] = meshgrid(linspace(-1, 1, n), linspace(-1, 1, n), linspace(-1, 1, n));
 
-methodnames = {'basic', 'olim6_rhr', 'olim18_rhr', 'olim26_rhr'};
+% methodnames = {'basic', 'olim6_rhr', 'olim18_rhr', 'olim26_rhr'};
+% methodnames = {'basic', 'olim6_mp0', 'olim18_mp0', 'olim26_mp0'};
+% methodnames = {'basic', 'olim6_mp1', 'olim18_mp1', 'olim26_mp1'};
 
 rows = 2;
 cols = 2;

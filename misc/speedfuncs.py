38408d28537622356a519b3b8d8669135f5be93c
Sam Potter <sfp@hush.ai>
2017-11-13 11:22:26 -0500

    add dimension inspection

--- misc/speedfuncs.py
+++ misc/speedfuncs.py
@@ -10,3 +10,18 @@ f2 = lambda x, y: np.power(x + y, 2)/(2*np.sqrt(2))
 
 s3 = r;
 f3 = lambda x, y: (np.power(x, 2) + np.power(y, 2))/2
+
+_speed_funcs = [s1, s2, s3]
+_soln_funcs = [f1, f2, f3]
+
+for s in _speed_funcs:
+    s.dim = 2
+
+for f in _soln_funcs:
+    f.dim = 2
+
+_soln_func_map = {
+    _speed_funcs[i]: _soln_funcs[i] for i in range(len(_speed_funcs))}
+
+def get_soln_func(s):
+    return _soln_func_map[s]

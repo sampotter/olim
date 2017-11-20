9e1dae333a7dc2d4b9e44ee0fb7aceca89e2d1a7
Sam Potter <sfp@hush.ai>
2017-11-19 22:23:49 -0500

    fix olim18 octant tests

--- test/olim18_mp0.test.cpp
+++ test/olim18_mp0.test.cpp
@@ -7,7 +7,7 @@ int main() {
   using olim3d = olim18_mp0;
 
   quadrants_are_correct<olim3d>(sqrt(2));
-  octants_are_correct<olim3d>(sqrt(2), 1.0 + 2/sqrt(3));
+  octants_are_correct<olim3d>(sqrt(2), sqrt(2) + 1.0/sqrt(3));
   planes_are_correct<olim, olim3d>();
   result_is_symmetric<olim3d>();
   two_by_two_by_three_cells_are_correct<olim3d>();

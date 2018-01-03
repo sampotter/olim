90a5a9261f454c503a2a9a5d526f4fe595aa6b6e
Sam Potter <sfp@hush.ai>
2018-01-03 13:15:12 -0500

    get these tetra tests working

--- test/update_rules.tetra_updates.test.cpp
+++ test/update_rules.tetra_updates.test.cpp
@@ -6,6 +6,34 @@
 
 static update_rules::rhr_tetra_updates updates;
 
+#define P001 1
+#define P010 2
+#define P011 3
+#define P100 4
+#define P101 5
+#define P110 6
+#define P111 7
+
+#define TETRA111(tetra_updates, u0, u1, u2, s, s0, s1, s2, h) \
+  tetra_updates.tetra(                                        \
+    u0, u1, u2, s, s0, s1, s2, h,                             \
+    ffvec<P001> {}, ffvec<P010> {}, ffvec<P100> {})
+
+#define TETRA122(tetra_updates, u0, u1, u2, s, s0, s1, s2, h) \
+  tetra_updates.tetra(                                        \
+    u0, u1, u2, s, s0, s1, s2, h,                             \
+    ffvec<P001> {}, ffvec<P011> {}, ffvec<P101> {})
+
+#define TETRA222(tetra_updates, u0, u1, u2, s, s0, s1, s2, h) \
+  tetra_updates.tetra(                                        \
+    u0, u1, u2, s, s0, s1, s2, h,                             \
+    ffvec<P011> {}, ffvec<P101> {}, ffvec<P110> {})
+
+#define TETRA123(tetra_updates, u0, u1, u2, s, s0, s1, s2, h) \
+  tetra_updates.tetra(                                        \
+    u0, u1, u2, s, s0, s1, s2, h,                             \
+    ffvec<P001> {}, ffvec<P011> {}, ffvec<P111> {})
+
 template <class rules>
 void tetra111_is_symmetric() {
   rules r;
@@ -15,12 +43,12 @@ void tetra111_is_symmetric() {
   double u2 = 2.2844570503761732;
   double s = 1, s0 = 1, s1 = 1, s2 = 1;
   double h = 1;
-  double val012 = r.tetra111(u0, u1, u2, s, s0, s1, s2, h);
-  double val120 = r.tetra111(u1, u2, u0, s, s0, s1, s2, h);
-  double val201 = r.tetra111(u2, u0, u1, s, s0, s1, s2, h);
-  double val210 = r.tetra111(u2, u1, u0, s, s0, s1, s2, h);
-  double val102 = r.tetra111(u1, u0, u2, s, s0, s1, s2, h);
-  double val021 = r.tetra111(u0, u2, u1, s, s0, s1, s2, h);
+  double val012 = TETRA111(r, u0, u1, u2, s, s0, s1, s2, h);
+  double val120 = TETRA111(r, u1, u2, u0, s, s0, s1, s2, h);
+  double val201 = TETRA111(r, u2, u0, u1, s, s0, s1, s2, h);
+  double val210 = TETRA111(r, u2, u1, u0, s, s0, s1, s2, h);
+  double val102 = TETRA111(r, u1, u0, u2, s, s0, s1, s2, h);
+  double val021 = TETRA111(r, u0, u2, u1, s, s0, s1, s2, h);
   IS_APPROX_EQUAL(val012, val120);
   IS_APPROX_EQUAL(val120, val201);
   IS_APPROX_EQUAL(val201, val210);
@@ -30,12 +58,12 @@ void tetra111_is_symmetric() {
   u0 = 2.4;
   u1 = 2.2;
   u2 = 2.0;
-  val012 = r.tetra111(u0, u1, u2, s, s0, s1, s2, h);
-  val120 = r.tetra111(u1, u2, u0, s, s0, s1, s2, h);
-  val201 = r.tetra111(u2, u0, u1, s, s0, s1, s2, h);
-  val210 = r.tetra111(u2, u1, u0, s, s0, s1, s2, h);
-  val102 = r.tetra111(u1, u0, u2, s, s0, s1, s2, h);
-  val021 = r.tetra111(u0, u2, u1, s, s0, s1, s2, h);
+  val012 = TETRA111(r, u0, u1, u2, s, s0, s1, s2, h);
+  val120 = TETRA111(r, u1, u2, u0, s, s0, s1, s2, h);
+  val201 = TETRA111(r, u2, u0, u1, s, s0, s1, s2, h);
+  val210 = TETRA111(r, u2, u1, u0, s, s0, s1, s2, h);
+  val102 = TETRA111(r, u1, u0, u2, s, s0, s1, s2, h);
+  val021 = TETRA111(r, u0, u2, u1, s, s0, s1, s2, h);
   IS_APPROX_EQUAL(val012, val120);
   IS_APPROX_EQUAL(val120, val201);
   IS_APPROX_EQUAL(val201, val210);
@@ -48,8 +76,8 @@ void tetra122_is_symmetric() {
   double u1 = 2.2;
   double u2 = 2.0;
   double s = 1, s0 = 1.0, s1 = 1.0, s2 = 1.0, h = 1;
-  double val012 = updates.tetra122(u0, u1, u2, s, s0, s1, s2, h);
-  double val021 = updates.tetra122(u0, u2, u1, s, s0, s1, s2, h);
+  double val012 = TETRA122(updates, u0, u1, u2, s, s0, s1, s2, h);
+  double val021 = TETRA122(updates, u0, u2, u1, s, s0, s1, s2, h);
   IS_APPROX_EQUAL(val012, val021);
 }
 
@@ -58,12 +86,12 @@ void tetra222_is_symmetric() {
   double u1 = 2.5453289254261224;
   double u2 = 2.2844570503761732;
   double s = 1, s0 = 1.0, s1 = 1.0, s2 = 1.0, h = 1;
-  double val012 = updates.tetra222(u0, u1, u2, s, s0, s1, s2, h);
-  double val120 = updates.tetra222(u1, u2, u0, s, s0, s1, s2, h);
-  double val201 = updates.tetra222(u2, u0, u1, s, s0, s1, s2, h);
-  double val210 = updates.tetra222(u2, u1, u0, s, s0, s1, s2, h);
-  double val102 = updates.tetra222(u1, u0, u2, s, s0, s1, s2, h);
-  double val021 = updates.tetra222(u0, u2, u1, s, s0, s1, s2, h);
+  double val012 = TETRA222(updates, u0, u1, u2, s, s0, s1, s2, h);
+  double val120 = TETRA222(updates, u1, u2, u0, s, s0, s1, s2, h);
+  double val201 = TETRA222(updates, u2, u0, u1, s, s0, s1, s2, h);
+  double val210 = TETRA222(updates, u2, u1, u0, s, s0, s1, s2, h);
+  double val102 = TETRA222(updates, u1, u0, u2, s, s0, s1, s2, h);
+  double val021 = TETRA222(updates, u0, u2, u1, s, s0, s1, s2, h);
   IS_APPROX_EQUAL(val012, val120);
   IS_APPROX_EQUAL(val120, val201);
   IS_APPROX_EQUAL(val201, val210);
@@ -73,12 +101,12 @@ void tetra222_is_symmetric() {
   u0 = 2.4;
   u1 = 2.2;
   u2 = 2.0;
-  val012 = updates.tetra222(u0, u1, u2, s, s0, s1, s2, h);
-  val120 = updates.tetra222(u1, u2, u0, s, s0, s1, s2, h);
-  val201 = updates.tetra222(u2, u0, u1, s, s0, s1, s2, h);
-  val210 = updates.tetra222(u2, u1, u0, s, s0, s1, s2, h);
-  val102 = updates.tetra222(u1, u0, u2, s, s0, s1, s2, h);
-  val021 = updates.tetra222(u0, u2, u1, s, s0, s1, s2, h);
+  val012 = TETRA222(updates, u0, u1, u2, s, s0, s1, s2, h);
+  val120 = TETRA222(updates, u1, u2, u0, s, s0, s1, s2, h);
+  val201 = TETRA222(updates, u2, u0, u1, s, s0, s1, s2, h);
+  val210 = TETRA222(updates, u2, u1, u0, s, s0, s1, s2, h);
+  val102 = TETRA222(updates, u1, u0, u2, s, s0, s1, s2, h);
+  val021 = TETRA222(updates, u0, u2, u1, s, s0, s1, s2, h);
   IS_APPROX_EQUAL(val012, val120);
   IS_APPROX_EQUAL(val120, val201);
   IS_APPROX_EQUAL(val201, val210);
@@ -94,7 +122,7 @@ void tetra111_works() {
     double s = 1.0, s0 = 1.0, s1 = 1.0, s2 = 1.0;
     double h = 1.0/7.0;
     double uhat = 1.95377665722661;
-    IS_APPROX_EQUAL(updates.tetra111(u0, u1, u2, s, s0, s1, s2, h), uhat);
+    IS_APPROX_EQUAL(TETRA111(updates, u0, u1, u2, s, s0, s1, s2, h), uhat);
   }
   {
     double u0 = 0.6714002359494359;
@@ -103,7 +131,7 @@ void tetra111_works() {
     double s = 1, s0 = 1.0, s1 = 1.0, s2 = 1.0;
     double h = 0.02040816326530612;
     double uhat = 0.6726606175825081;
-    IS_APPROX_EQUAL(updates.tetra111(u0, u1, u2, s, s0, s1, s2, h), uhat);
+    IS_APPROX_EQUAL(TETRA111(updates, u0, u1, u2, s, s0, s1, s2, h), uhat);
   }
   {
     double u0 = 0.8701508258299168;
@@ -112,25 +140,25 @@ void tetra111_works() {
     double s = 1, s0 = 1.0, s1 = 1.0, s2 = 1.0;
     double h = 0.03703703703703703;
     double uhat = 0.9064782064785435;
-    IS_APPROX_EQUAL(updates.tetra111(u0, u1, u2, s, s0, s1, s2, h), uhat);
+    IS_APPROX_EQUAL(TETRA111(updates, u0, u1, u2, s, s0, s1, s2, h), uhat);
   }
 }
 
 void tetra123_works() {
-  IS_APPROX_EQUAL(updates.tetra123(0, 0, 0, 1, 1, 1, 1, 1), 1.0);
+  IS_APPROX_EQUAL(TETRA123(updates, 0, 0, 0, 1, 1, 1, 1, 1), 1.0);
 }
 
 template <class rules>
 void tetra222_works() {
   rules r;
-  IS_APPROX_EQUAL(r.tetra222(0, 0, 0, 1, 1, 1, 1, 1), 2.0/sqrt(3));
+  IS_APPROX_EQUAL(TETRA222(r, 0, 0, 0, 1, 1, 1, 1, 1), 2.0/sqrt(3));
   {
     double u0 = 0.18181818181818182;
     double u1 = 0.33244129540839806;
     double u2 = 0.33244129540839812;
     double h = 0.18181818181818182;
-    double U = INF(double);
-    IS_APPROX_EQUAL(r.tetra222(u0, u1, u2, 1, 1, 1, 1, h), U);
+    double U = 0.4389479204314718;
+    IS_APPROX_EQUAL(TETRA222(r, u0, u1, u2, 1, 1, 1, 1, h), U);
   }
 }
 
@@ -141,7 +169,7 @@ void mp1_tetra123_works() {
 
   u0 = 0.889163, u1 = 0.817579, u2 = 0.75, s = 1, s0 = 1, s1 = 1, s2 = 1,
     h = 0.25, U = 1.1189646747175703;
-  IS_APPROX_EQUAL(mp1.tetra123(u0, u1, u2, s, s0, s1, s2, h), U);
+  IS_APPROX_EQUAL(TETRA123(mp1, u0, u1, u2, s, s0, s1, s2, h), U);
 }
 
 int main() {

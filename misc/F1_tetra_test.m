c2b3fa0b33193a579747b52abe5be23854ef8a39
Sam Potter <sfp@hush.ai>
2018-01-03 12:57:46 -0500

    fix

--- misc/F1_tetra_test.m
+++ misc/F1_tetra_test.m
@@ -2,11 +2,11 @@ clear;
 
 F0_tetra_setup;
 
-s = @(x) (1 - theta)*s + theta*((1 - sum(x))*s0 + x(1)*s1 + x(2)*s2);
+stheta = @(x) (1 - theta)*s + theta*((1 - sum(x))*s0 + x(1)*s1 + x(2)*s2);
 
-F1 = @(x) u(x) + h*s(x)*l(x);
-dF1 = @(x) du + h*(theta*q(x)*ds + s(x)*dP'*p(x))/l(x);
-d2F1 = @(x) h*(theta*(dP'*p(x)*ds' + ds*p(x)'*dP) + s(x)*dP'*cprojp(x)*dP)/l(x);
+F1 = @(x) u(x) + h*stheta(x)*l(x);
+dF1 = @(x) du + h*(theta*q(x)*ds + stheta(x)*dP'*p(x))/l(x);
+d2F1 = @(x) h*(theta*(dP'*p(x)*ds' + ds*p(x)'*dP) + stheta(x)*dP'*cprojp(x)*dP)/l(x);
 
 out = sqp(F1, dF1, d2F1, -A, -b, ones(N - 1, 1)/N, eps, 100);
 

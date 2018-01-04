7f48bb0f56825e16994d53a6c66aab8453f719eb
Sam Potter <sfp@hush.ai>
2018-01-04 18:59:50 -0500

    simplify this using a template class

--- src/olim3d.hpp
+++ src/olim3d.hpp
@@ -70,47 +70,30 @@ using olim3d_rhr = olim3d<
   update_rules::rhr_tetra_updates,
   groups>;
 
-struct olim6_groups {
-  static constexpr bool group_I = false;
-  static constexpr bool group_II = false;
-  static constexpr bool group_III = false;
-  static constexpr bool group_IV_a = true;
-  static constexpr bool group_IV_b = false;
-  static constexpr bool group_V = false;
-  static constexpr bool group_VI_a = false;
-  static constexpr bool group_VI_b = false;
+template <bool I, bool II, bool III, bool IV_a, bool IV_b, bool V,
+          bool VI_a, bool VI_b>
+struct groups_t {
+  static constexpr bool group_I = I;
+  static constexpr bool group_II = II;
+  static constexpr bool group_III = III;
+  static constexpr bool group_IV_a = IV_a;
+  static constexpr bool group_IV_b = IV_b;
+  static constexpr bool group_V = V;
+  static constexpr bool group_VI_a = VI_a;
+  static constexpr bool group_VI_b = VI_b;
 };
 
+using olim6_groups = groups_t<0, 0, 0, 1, 0, 0, 0, 0>;
 using olim6_mp0 = olim3d_mp0<olim6_groups>;
 using olim6_mp1 = olim3d_mp1<olim6_groups>;
 using olim6_rhr = olim3d_rhr<olim6_groups>;
 
-struct olim18_groups {
-  static constexpr bool group_I = true;
-  static constexpr bool group_II = true;
-  static constexpr bool group_III = true;
-  static constexpr bool group_IV_a = true;
-  static constexpr bool group_IV_b = true;
-  static constexpr bool group_V = false;
-  static constexpr bool group_VI_a = false;
-  static constexpr bool group_VI_b = false;
-};
-
+using olim18_groups = groups_t<1, 0, 0, 1, 1, 0, 0, 0>;
 using olim18_mp0 = olim3d_mp0<olim18_groups>;
 using olim18_mp1 = olim3d_mp1<olim18_groups>;
 using olim18_rhr = olim3d_rhr<olim18_groups>;
 
-struct olim26_groups {
-  static constexpr bool group_I = true;
-  static constexpr bool group_II = false;
-  static constexpr bool group_III = false;
-  static constexpr bool group_IV_a = true;
-  static constexpr bool group_IV_b = true;
-  static constexpr bool group_V = true;
-  static constexpr bool group_VI_a = true;
-  static constexpr bool group_VI_b = true;
-};
-
+using olim26_groups = groups_t<1, 0, 0, 1, 1, 1, 1, 1>;
 using olim26_mp0 = olim3d_mp0<olim26_groups>;
 using olim26_mp1 = olim3d_mp1<olim26_groups>;
 using olim26_rhr = olim3d_rhr<olim26_groups>;

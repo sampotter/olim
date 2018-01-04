71644ae48c9a498e39a9e1fc48ac390f416a679d
Sam Potter <sfp@hush.ai>
2018-01-04 14:20:17 -0500

    finish this---plots now

--- misc/sphere3d.py
+++ misc/sphere3d.py
@@ -5,14 +5,20 @@ sys.path.insert(0, '../build/Release')
 import eikonal as eik
 import matplotlib.pyplot as plt
 import numpy as np
-import skimage
+from mpl_toolkits.mplot3d import Axes3D
+from skimage import measure
 
 if __name__ == '__main__':
-    n = 2**5 + 1
-    m = eik.BasicMarcher3D(n, n, n, h=2/(n - 1), x0=1, y0=1, z0=1)
+    n = 2**4 + 1
+    m = eik.Olim26Rect(n, n, n, h=2/(n - 1), x0=1, y0=1, z0=1)
     i = int((n - 1)/2)
     m.addBoundaryNode(i, i, i)
     m.run()
+
     U = np.array(m)
-    verts, faces = skimage.measure.marching_cubes(U, 1)
+    verts, faces, normals, values = measure.marching_cubes(U, 1)
     
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(verts[:, 0], verts[:, 1], faces, verts[:, 2])
+    plt.show()

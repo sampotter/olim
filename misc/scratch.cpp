026cf58a74cc0afcdde87782673926fe55d2ceea
Sam Potter <sfp@hush.ai>
2018-01-10 20:36:38 -0500

    modified scratch

--- misc/scratch.cpp
+++ misc/scratch.cpp
@@ -1,8 +1,8 @@
-#include <olim.hpp>
+#include <olim3d.hpp>
 
 int main() {
-  int n = 1001;
-  olim8_mp1 m {n, n, 2./(n-1), (speed_func) default_speed_func, 1., 1.};
-  m.add_boundary_node(n/2, n/2);
+  int n = 11;
+  olim18_rhr m {n, n, n, 2./(n-1), (speed_func_3d) default_speed_func, 1., 1., 1.};
+  m.add_boundary_node(n/2, n/2, n/2);
   m.run();
 }

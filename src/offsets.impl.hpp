479131a5b60c85af87734c2d07ad833fb99f5397
Sam Potter <sfp@hush.ai>
2018-01-21 22:56:32 -0500

    fix comment

--- src/offsets.impl.hpp
+++ src/offsets.impl.hpp
@@ -1,6 +1,9 @@
 #ifndef __OFFSETS_IMPL_HPP__
 #define __OFFSETS_IMPL_HPP__
 
+// neighbor order: N, E, S, W, NE, SE, SW, NW (ordered first by
+// degree, then clockwise)
+
 template <> int di<2>[] = {-1, 0, 1, 0, -1, 1, 1, -1};
 
 template <> int dj<2>[] = {0, 1, 0, -1, 1, 1, -1, -1};

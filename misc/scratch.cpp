b67c7b302a0b4ba071e82e317b7dd18c7850382e
Sam Potter <sfp@hush.ai>
2018-01-21 21:08:31 -0500

    2D and 3D scratch

--- misc/scratch.cpp
+++ misc/scratch.cpp
@@ -1,8 +1,14 @@
+#include <olim.hpp>
 #include <olim3d.hpp>
 
 int main() {
-  int n = 11;
-  olim18_rhr m {n, n, n, 2./(n-1), (speed_func_3d) default_speed_func, 1., 1., 1.};
-  m.add_boundary_node(n/2, n/2, n/2);
+  // int n = 25;
+  // olim18_rhr m {n, n, n, 2./(n-1), (speed_func_3d) default_speed_func, 1., 1., 1.};
+  // m.add_boundary_node(n/2, n/2, n/2);
+  // m.run();
+
+  int n = 2001;
+  olim8_rhr m {n, n, 2./(n-1), (speed_func) default_speed_func, 1., 1.};
+  m.add_boundary_node(n/2, n/2);
   m.run();
 }

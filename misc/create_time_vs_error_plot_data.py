0922a35035af5aeb84f60e9457a3852c16aef114
Sam Potter <sfp@hush.ai>
2018-02-27 19:32:24 -0500

    use parallel HDF5 features to speed this up

--- misc/create_time_vs_error_plot_data.py
+++ misc/create_time_vs_error_plot_data.py
@@ -6,8 +6,8 @@ sys.path.insert(0, '../build/Release')
 
 import argparse
 import eikonal as eik
-import glob
 import h5py
+import mpi4py.MPI
 import numpy as np
 import os.path
 
@@ -16,6 +16,10 @@ from common3d import compute_soln, get_exact_soln, get_marcher_name, marchers, \
 from itertools import product
 from speedfuncs3d import get_speed_func_name, get_speed_func_by_name, \
     get_soln_func, speed_funcs
+
+comm = mpi4py.MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 def rms(x):
     y = x.flatten()
@@ -38,20 +42,51 @@ def get_dataset_name(Marcher, s):
     sname = get_speed_func_name(s)
     return '%s/%s' % (mname.replace(' ', '_'), sname)
 
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('path', type=str)
+    p.add_argument('-m', '--minpow', type=int, default=3)
+    p.add_argument('-M', '--maxpow', type=int, default=7)
+    p.add_argument('-s', '--step', type=int, default=2)
+    p.add_argument('--speed_funcs', type=str)
+    return p.parse_args()
+
+def create_datasets(f, M_by_s, ns):
+    for Marcher, s in M_by_s:
+        name = get_dataset_name(Marcher, s)
+        for n in ns:
+            shape = (n, n, n)
+            f.create_dataset(name + '/u' + str(n), shape, dtype=np.float)
+            f.create_dataset(name + '/U' + str(n), shape, dtype=np.float)
+        f.create_dataset(name + '/rms', (len(ns),), dtype=np.float)
+        f.create_dataset(name + '/max', (len(ns),), dtype=np.float)
+        f.create_dataset(name + '/t', (len(ns),), dtype=np.float)
+
+def collect_results(Marcher, s, ns):
+    name = get_dataset_name(Marcher, s)
+    print(name)
+
+    print('- computing exact solutions')
+    us = [get_exact_soln(get_soln_func(s), n) for n in ns]
+    for n, u in zip(ns, us):
+        f[name + '/u' + str(n)][:, :, :] = u
+
+    print('- computing numerical solutions')
+    Us = [compute_soln(Marcher, s, n) for n in ns]
+    for n, U in zip(ns, Us):
+        f[name + '/U' + str(n)][:, :, :] = U
+
+    print('- evaluating errors')
+    f[name + '/rms'][:] = [rms(u - U) for u, U in zip(us, Us)]
+    f[name + '/max'][:] = [linf_error(u - U) for u, U in zip(us, Us)]
+
+    print('- collecting CPU times')
+    f[name + '/t'][:] = [time_marcher(Marcher, s, n) for n in ns]
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--minpow', type=int, default=3)
-    parser.add_argument('--maxpow', type=int, default=7)
-    parser.add_argument('--step', type=int, default=2)
-    parser.add_argument('--hdf5_path', type=str, default='time_vs_error.hdf5')
-    parser.add_argument('--speed_funcs', type=str)
-    args = parser.parse_args()
+    args = parse_args()
 
-    path = args.hdf5_path
-
-    with h5py.File(path, 'w') as f:
-        ns = get_ns(args)
-        print(list(ns))
+    with h5py.File(args.path, 'w', driver='mpio', comm=comm) as f:
 
         if args.speed_funcs is not None:
             speed_funcs_ = [
@@ -60,27 +95,15 @@ if __name__ == '__main__':
         else:
             speed_funcs_ = speed_funcs()
 
-        prod = product(marchers, speed_funcs_)
-        for M, s in prod:
-            name = get_dataset_name(M, s)
-            print(name)
+        ns = get_ns(args)
+        if rank == 0:
+            print('Test problem sizes: ' + ', '.join(map(str, ns)))
 
-            print('- computing exact solutions')
-            us = [get_exact_soln(get_soln_func(s), n) for n in ns]
-            for n, u in zip(ns, us):
-                f.create_dataset(name + '/u' + str(n), data=u)
+        if rank == 0:
+            print('Creating datasets')
+        create_datasets(f, product(marchers, speed_funcs_), ns)
 
-            print('- computing numerical solutions')
-            Us = [compute_soln(M, s, n) for n in ns]
-            for n, U in zip(ns, Us):
-                f.create_dataset(name + '/U' + str(n), data=U)
-
-            print('- evaluating errors')
-            RMSs = [rms(u - U) for u, U in zip(us, Us)]
-            f.create_dataset(name + '/rms', data=RMSs)
-            max_errors = [linf_error(u - U) for u, U in zip(us, Us)]
-            f.create_dataset(name + '/max', data=max_errors)
-
-            print('- collecting CPU times')
-            Ts = [time_marcher(M, s, n) for n in ns]
-            f.create_dataset(name + '/t', data=Ts)
+        for i, (Marcher, s) in enumerate(product(marchers, speed_funcs_)):
+            if i % size != rank:
+                continue
+            collect_results(Marcher, s, ns)

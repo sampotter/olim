d5998fe6ae6d4e29851dce49868ab28ce4106c09
Sam Potter <sfp@hush.ai>
2018-03-19 16:48:09 -0400

    collect stats in scratch file

--- misc/scratch.cpp
+++ misc/scratch.cpp
@@ -2,13 +2,14 @@
 #include <olim3d.hpp>
 
 int main() {
-  // int n = 25;
-  // olim18_rhr m {n, n, n, 2./(n-1), (speed_func_3d) default_speed_func, 1., 1., 1.};
-  // m.add_boundary_node(n/2, n/2, n/2);
-  // m.run();
-
-  int n = 2001;
-  olim8_rhr m {n, n, 2./(n-1), (speed_func) default_speed_func, 1., 1.};
-  m.add_boundary_node(n/2, n/2);
+  int n = 11;
+  olim26_mp1 m {n, n, n, 2./(n-1), (speed_func_3d) s4, 1., 1., 1.};
+  m.add_boundary_node(n/2, n/2, n/2);
   m.run();
+  m.dump_stats();
+
+  // int n = 2001;
+  // olim8_rhr m {n, n, 2./(n-1), (speed_func) default_speed_func, 1., 1.};
+  // m.add_boundary_node(n/2, n/2);
+  // m.run();
 }

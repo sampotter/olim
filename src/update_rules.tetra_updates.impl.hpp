a96f775405344c632151ee2fd3bfb6cfd282e091
Sam Potter <sfp@hush.ai>
2018-03-22 16:42:55 -0400

    todo comments

--- src/update_rules.tetra_updates.impl.hpp
+++ src/update_rules.tetra_updates.impl.hpp
@@ -42,11 +42,14 @@ double update_rules::tetra_updates<derived>::tetra(
   double u0, double u1, double u2, double s,
   double s0, double s1, double s2, double h) const
 {
+  // TODO: we want to avoid having to pack everything into these
+  // arrays, this is a waste
   double u[3] = {u0, u1, u2};
   double s_hat = s;
   double s_[3] = {s0, s1, s2};
   double p[3][3];
 
+  // TODO: we want to avoid doing this, this is totally unnecessary
   memcpy((void *) p[0], (void *) p0, 3*sizeof(double));
   memcpy((void *) p[1], (void *) p1, 3*sizeof(double));
   memcpy((void *) p[2], (void *) p2, 3*sizeof(double));
@@ -62,6 +65,8 @@ double update_rules::tetra_updates<derived>::tetra(
   sqp(func, lambda, &error);
   assert(!error);
 
+  // TODO: awful hack for now---need to fix the way we've organized
+  // the cost functions
   if (std::is_same<derived, mp0_tetra_updates>::value) {
     F1<3, 2> eval_func {h, static_cast<derived const *>(this)->theta()};
     eval_func.set_args(u, s_hat, s_, p);

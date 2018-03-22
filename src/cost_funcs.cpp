9c9ce8c21655e1b7042a501fc1bae889c1378890
Sam Potter <sfp@hush.ai>
2018-03-22 16:42:02 -0400

    better formatting

--- src/cost_funcs.cpp
+++ src/cost_funcs.cpp
@@ -27,11 +27,12 @@ F0<3, 2>::hess_impl(double d2f[2][2]) const
 {
   double tmp[6]; // workspace
   tmp[0] = 1 - _p_lam[0]*_p_lam[0]/_q; // (0, 0)
-  tmp[1] = -_p_lam[0]*_p_lam[1]/_q; // (0, 1) & (1, 0)
-  tmp[2] = -_p_lam[0]*_p_lam[2]/_q; // (0, 2) & (2, 0)
+  tmp[1] = -_p_lam[0]*_p_lam[1]/_q;    // (0, 1) & (1, 0)
+  tmp[2] = -_p_lam[0]*_p_lam[2]/_q;    // (0, 2) & (2, 0)
   tmp[3] = 1 - _p_lam[1]*_p_lam[1]/_q; // (1, 1)
-  tmp[4] = -_p_lam[1]*_p_lam[2]/_q; // (1, 2) & (2, 1)
+  tmp[4] = -_p_lam[1]*_p_lam[2]/_q;    // (1, 2) & (2, 1)
   tmp[5] = 1 - _p_lam[2]*_p_lam[2]/_q; // (2, 2)
+
   double tmp2[2][3];
   tmp2[0][0] = tmp[0]*_dP[0][0] + tmp[1]*_dP[0][1] + tmp[2]*_dP[0][2];
   tmp2[0][1] = tmp[1]*_dP[0][0] + tmp[3]*_dP[0][1] + tmp[4]*_dP[0][2];
@@ -39,9 +40,11 @@ F0<3, 2>::hess_impl(double d2f[2][2]) const
   tmp2[1][0] = tmp[0]*_dP[1][0] + tmp[1]*_dP[1][1] + tmp[2]*_dP[1][2];
   tmp2[1][1] = tmp[1]*_dP[1][0] + tmp[3]*_dP[1][1] + tmp[4]*_dP[1][2];
   tmp2[1][2] = tmp[2]*_dP[1][0] + tmp[4]*_dP[1][1] + tmp[5]*_dP[1][2];
+
   tmp[0] = _dP[0][0]*tmp2[0][0] + _dP[0][1]*tmp2[0][1] + _dP[0][2]*tmp2[0][2];
   tmp[1] = _dP[0][0]*tmp2[1][0] + _dP[0][1]*tmp2[1][1] + _dP[0][2]*tmp2[1][2];
   tmp[2] = _dP[1][0]*tmp2[1][0] + _dP[1][1]*tmp2[1][1] + _dP[1][2]*tmp2[1][2];
+
   d2f[0][0] = _sh*tmp[0]/_l;
   d2f[0][1] = d2f[1][0] = _sh*tmp[1]/_l;
   d2f[1][1] = _sh*tmp[2]/_l;
@@ -104,11 +107,12 @@ F1<3, 2>::hess_impl(double d2f[2][2]) const
 {
   double tmp[6]; // workspace
   tmp[0] = 1 - _p_lam[0]*_p_lam[0]/_q; // (0, 0)
-  tmp[1] = -_p_lam[0]*_p_lam[1]/_q; // (0, 1) & (1, 0)
-  tmp[2] = -_p_lam[0]*_p_lam[2]/_q; // (0, 2) & (2, 0)
+  tmp[1] = -_p_lam[0]*_p_lam[1]/_q;    // (0, 1) & (1, 0)
+  tmp[2] = -_p_lam[0]*_p_lam[2]/_q;    // (0, 2) & (2, 0)
   tmp[3] = 1 - _p_lam[1]*_p_lam[1]/_q; // (1, 1)
-  tmp[4] = -_p_lam[1]*_p_lam[2]/_q; // (1, 2) & (2, 1)
+  tmp[4] = -_p_lam[1]*_p_lam[2]/_q;    // (1, 2) & (2, 1)
   tmp[5] = 1 - _p_lam[2]*_p_lam[2]/_q; // (2, 2)
+
   double tmp2[2][3];
   tmp2[0][0] = tmp[0]*_dP[0][0] + tmp[1]*_dP[0][1] + tmp[2]*_dP[0][2];
   tmp2[0][1] = tmp[1]*_dP[0][0] + tmp[3]*_dP[0][1] + tmp[4]*_dP[0][2];
@@ -116,17 +120,21 @@ F1<3, 2>::hess_impl(double d2f[2][2]) const
   tmp2[1][0] = tmp[0]*_dP[1][0] + tmp[1]*_dP[1][1] + tmp[2]*_dP[1][2];
   tmp2[1][1] = tmp[1]*_dP[1][0] + tmp[3]*_dP[1][1] + tmp[4]*_dP[1][2];
   tmp2[1][2] = tmp[2]*_dP[1][0] + tmp[4]*_dP[1][1] + tmp[5]*_dP[1][2];
+
   // compute dP'*(I - p*p'/q)*dP
   tmp[0] = _dP[0][0]*tmp2[0][0] + _dP[0][1]*tmp2[0][1] + _dP[0][2]*tmp2[0][2];
   tmp[1] = _dP[0][0]*tmp2[1][0] + _dP[0][1]*tmp2[1][1] + _dP[0][2]*tmp2[1][2];
   tmp[2] = _dP[1][0]*tmp2[1][0] + _dP[1][1]*tmp2[1][1] + _dP[1][2]*tmp2[1][2];
+
   // compute dP'*p
   tmp[3] = _dP[0][0]*_p_lam[0] + _dP[0][1]*_p_lam[1] + _dP[0][2]*_p_lam[2];
   tmp[4] = _dP[1][0]*_p_lam[0] + _dP[1][1]*_p_lam[1] + _dP[1][2]*_p_lam[2];
+
   // compute theta*(dP'*p*ds' + ds*p'*dP)
   tmp2[0][0] = 2*_theta*tmp[3]*_ds[0];
   tmp2[0][1] = _theta*(tmp[3]*_ds[1] + tmp[4]*_ds[0]);
   tmp2[0][2] = 2*_theta*tmp[4]*_ds[1];
+
   // compute Hessian
   d2f[0][0] = _h*(tmp2[0][0] + _stheta*tmp[0])/_l;
   d2f[0][1] = d2f[1][0] = _h*(tmp2[0][1] + _stheta*tmp[1])/_l;

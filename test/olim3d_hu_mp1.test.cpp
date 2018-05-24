30eab5e5d4ef36f361b2c828457cff25fbbe26c9
Sam Potter <sfp@hush.ai>
2018-05-24 18:32:56 -0400

    add some more olim3d_hu_* tests

--- test/olim3d_hu_mp1.test.cpp
+++ test/olim3d_hu_mp1.test.cpp
@@ -1,9 +1,19 @@
+#include "olim.hpp"
 #include "olim3d.hpp"
 #include "olim.test.common.hpp"
 
+using olim_t = olim8_mp1;
 using olim3d_t = olim3d_hu_mp1;
 
 TEST (olim3d_hu_mp1, quadrants_are_correct) {
   quadrants_are_correct<olim3d_t>(sqrt(2));
 }
 
+TEST (olim3d_hu_mp1, octants_are_correct) {
+  octants_are_correct<olim3d_t>(sqrt(2), sqrt(3));
+}
+
+TEST (olim3d_hu_mp1, planes_are_correct) {
+  planes_are_correct<olim_t, olim3d_t>(speed_funcs[0], speed_funcs_3d[0]);
+  planes_are_correct<olim_t, olim3d_t>(speed_funcs[1], speed_funcs_3d[1], 9);
+}
--- test/olim3d_hu_rhr.test.cpp
+++ test/olim3d_hu_rhr.test.cpp
@@ -1,9 +1,19 @@
+#include "olim.hpp"
 #include "olim3d.hpp"
 #include "olim.test.common.hpp"
 
+using olim_t = olim8_rhr;
 using olim3d_t = olim3d_hu_rhr;
 
 TEST (olim3d_hu_rhr, quadrants_are_correct) {
   quadrants_are_correct<olim3d_t>(sqrt(2));
 }
 
+TEST (olim3d_hu_rhr, octants_are_correct) {
+  octants_are_correct<olim3d_t>(sqrt(2), sqrt(3));
+}
+
+TEST (olim3d_hu_rhr, planes_are_correct) {
+  planes_are_correct<olim_t, olim3d_t>(speed_funcs[0], speed_funcs_3d[0]);
+  planes_are_correct<olim_t, olim3d_t>(speed_funcs[1], speed_funcs_3d[1], 9);
+}

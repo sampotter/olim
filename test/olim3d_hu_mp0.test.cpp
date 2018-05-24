a4da6442cc78e841f950365bde89d92d73012a51
Sam Potter <sfp@hush.ai>
2018-05-24 18:38:11 -0400

    add the rest of the olim3d_hu_* tests (wip: some of these don't make sense)

--- test/olim3d_hu_mp0.test.cpp
+++ test/olim3d_hu_mp0.test.cpp
@@ -17,3 +17,25 @@ TEST (olim3d_hu_mp0, planes_are_correct) {
   planes_are_correct<olim_t, olim3d_t>(speed_funcs[0], speed_funcs_3d[0]);
   planes_are_correct<olim_t, olim3d_t>(speed_funcs[1], speed_funcs_3d[1], 9);
 }
+
+TEST (olim3d_hu_mp0, result_is_symmetric) {
+  for (int i = 0; i < 2; ++i) {
+    result_is_symmetric<olim3d_t>(speed_funcs_3d[i]);
+  }
+}
+
+TEST (olim3d_hu_mp0, two_by_two_by_three_cells_are_correct) {
+  two_by_two_by_three_cells_are_correct<olim3d_t>();
+}
+
+TEST (olim3d_hu_mp0, plane_boundaries_are_correct) {
+  plane_boundaries_are_correct<olim3d_t>();
+}
+
+TEST (olim3d_hu_mp0, planes_are_correct_nonsymmetric) {
+  planes_are_correct_nonsymmetric<olim_t, olim3d_t>(s4, f4xy, f4yz, f4xz);
+}
+
+TEST (olim3d_hu_mp0, planes_agree_nonsymmetric) {
+  planes_agree_nonsymmetric<olim_t, olim3d_t>(s4, s4xy, s4yz, s4xz);
+}

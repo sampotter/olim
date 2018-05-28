89ef27f4806d53a94f5fbb70a75926dcd572e32a
Sam Potter <sfp@hush.ai>
2018-05-28 17:41:04 -0400

    update scratch

--- misc/scratch.cpp
+++ misc/scratch.cpp
@@ -2,8 +2,12 @@
 #include <olim3d.hpp>
 #include <src/config.hpp>
 
-int main() {
-  int n = 65;
+int main(int argc, char * argv[]) {
+  if (argc < 2) {
+    std::exit(1);
+  }
+  int n = atoi(argv[1]);
+  // olim3d_hu_mp1 m {n, n, n, 2./(n-1), (speed_func_3d) s4, 1., 1., 1.};
   olim26_mp1 m {n, n, n, 2./(n-1), (speed_func_3d) s4, 1., 1., 1.};
   m.add_boundary_node(n/2, n/2, n/2);
   m.run();

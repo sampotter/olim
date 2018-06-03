c4964afdba5ea3d7a7eef0edddf0e55602b071ba
Sam Potter <sfp@hush.ai>
2018-06-03 09:01:45 -0400

    update scratch.cpp

--- misc/scratch.cpp
+++ misc/scratch.cpp
@@ -1,22 +1,29 @@
 #include <olim.hpp>
 #include <olim3d.hpp>
 #include <src/config.hpp>
+#include <string>
 
 int main(int argc, char * argv[]) {
-  if (argc < 2) {
-    std::exit(1);
+  if (argc < 3) {
+    std::exit(0);
   }
-  int n = atoi(argv[1]);
-  // olim3d_hu_mp1 m {n, n, n, 2./(n-1), (speed_func_3d) s4, 1., 1., 1.};
-  olim26_mp1 m {n, n, n, 2./(n-1), (speed_func_3d) s4, 1., 1., 1.};
-  m.add_boundary_node(n/2, n/2, n/2);
-  m.run();
-#if COLLECT_STATS
-  m.dump_stats();
-#endif
 
-  // int n = 2001;
-  // olim8_rhr m {n, n, 2./(n-1), (speed_func) default_speed_func, 1., 1.};
-  // m.add_boundary_node(n/2, n/2);
-  // m.run();
+  std::string marcher_name {argv[1]};
+  int n = atoi(argv[2]);
+
+  if (marcher_name == "olim3d_hu_mp1") {
+    olim3d_hu_mp1 m {n, n, n, 2./(n-1), (speed_func_3d) s4, 1., 1., 1.};
+    m.add_boundary_node(n/2, n/2, n/2);
+    m.run();
+#if COLLECT_STATS
+    m.dump_stats();
+#endif
+  } else if (marcher_name == "olim26_mp1") {
+    olim26_mp1 m {n, n, n, 2./(n-1), (speed_func_3d) s4, 1., 1., 1.};
+    m.add_boundary_node(n/2, n/2, n/2);
+    m.run();
+#if COLLECT_STATS
+    m.dump_stats();
+#endif
+  }
 }

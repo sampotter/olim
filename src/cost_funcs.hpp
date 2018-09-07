d292ef232c0284a47c1fa2d3f5f330a33db817a6
Sam Potter <sfp@hush.ai>
2018-09-07 17:41:31 -0400

    starting to break up these classes (wip)

--- src/cost_funcs.hpp
+++ src/cost_funcs.hpp
@@ -15,94 +15,83 @@
 
 #define __sym_mat_size(d) ((d*(d + 1))/2)
 
-/**
- * An explanation of the template arguments:
- * - derived: the effective derived subclass (cost_func uses CRTP)
- * - n: the dimension of the ambient space (where each p_i lives)
- * - d: the dimension of the domain of the cost function
- */
-template <class derived, int n, int d>
-struct cost_func {
-  inline void eval(double & f) const {
-    static_cast<derived const *>(this)->eval_impl(f);
-    __check(f);
-  }
+template <int n, int d>
+struct F0_wkspc {
+  double sh;
+  double u0;
+  double du[d];
+  double u_lam;
+  double l;
+  double q;
+  double p_lam[n];
+  double p0[n];
+  double dP[d][n];
+  double h; // TODO: delete?
+  double theta; // TODO: delete?
+};
 
-  inline void grad(double df[d]) const {
-    static_cast<derived const *>(this)->grad_impl(df);
-    for (int i = 0; i < d; ++i) {
-      __check(df[i]);
-    }
-  }
+template <int d, char... ps>
+struct F0_bv_wkspc;
 
-  inline void hess(double d2f[__sym_mat_size(d)]) const {
-    static_cast<derived const *>(this)->hess_impl(d2f);
-    for (int i = 0; i < __sym_mat_size(d); ++i) {
-      __check(d2f[i]);
-    }
-  }
-
-  void lag_mult(double const lambda[d], double * mu, int * k);
-
-  inline void set_lambda(double const lambda[d]) {
-    static_cast<derived *>(this)->set_lambda_impl(lambda);
-  }
-
-  inline void set_args(double const u[d + 1],
-                       double s_hat,
-                       double const s[d + 1],
-                       double const p[d + 1][n]) {
-    static_cast<derived *>(this)->set_args_impl(u, s_hat, s, p);
-  }
+template <int d>
+struct F0_bv_wkspc<d, char, char, char> {
+  double sh;
+  double u0;
+  double du[d];
+  double u_lam;
+  double l;
+  double y[d]; // TODO: more sensical name
+  double h; // TODO: delete?
+  double theta; // TODO: delete?
 };
 
 template <int n, int d>
-struct F0: public cost_func<F0<n, d>, n, d> {
-  F0(double h, double theta): _h {h}, _theta {theta} {}
-  void eval_impl(double & f) const;
-  void grad_impl(double df[d]) const;
-  void hess_impl(double d2f[__sym_mat_size(d)]) const;
-  void set_lambda_impl(double const lambda[d]);
-  void set_args_impl(double const u[d + 1], double s_hat,
-                     double const s[d + 1], double const p[d + 1][n]);
-EIKONAL_PROTECTED:
-  double _sh;
-  double _u0;
-  double _du[d];
-  double _u_lam;
-  double _l;
-  double _q;
-  double _p_lam[n];
-  double _p0[n];
-  double _dP[d][n];
-  double _h;
-  double _theta;
+struct F1_wkspc {
+  double s_hat;
+  double stheta;
+  double s[d + 1];
+  double ds[d];
+  double q;
+  double l;
+  double u_lam;
+  double u0;
+  double du[d];
+  double p_lam[n];
+  double p0[n];
+  double dP[d][n];
+  double h;
+  double theta;
+};
+
+template <int d, char... ps>
+struct F1_bv_wkspc;
+
+template <int d>
+struct F1_bv_wkspc<d, char, char, char> {
+  double s_hat;
+  double sh;
+  double s0;
+  double ds[d];
+  double l;
+  double u_lam;
+  double u0;
+  double du[d];
+  double y[d]; // TODO: more sensical name
+  double h; // TODO: delete?
+  double theta; // TODO: delete?
 };
 
 template <int n, int d>
-struct F1: public cost_func<F1<n, d>, n, d> {
-  F1(double h, double theta): _h {h}, _theta {theta} {}
-  void eval_impl(double & f) const;
-  void grad_impl(double df[d]) const;
-  void hess_impl(double d2f[__sym_mat_size(d)]) const;
-  void set_lambda_impl(double const lambda[d]);
-  void set_args_impl(double const u[d + 1], double s_hat,
-                     double const s[d + 1], double const p[d + 1][n]);
-EIKONAL_PROTECTED:
-  double _s_hat;
-  double _stheta;
-  double _s[d + 1];
-  double _ds[d];
-  double _q;
-  double _l;
-  double _u_lam;
-  double _u0;
-  double _du[d];
-  double _p_lam[n];
-  double _p0[n];
-  double _dP[d][n];
-  double _h;
-  double _theta;
+struct fac_wkspc {
+  double p_fac[n];
+  double s_fac;
+  double tau0;
+  double dtau[d];
+  double tau_lam;
+  double dPt_dot_p_fac[d];
+  double p_lam_minus_p_fac[n];
+  double l_fac_lam;
+  double l_fac_lam_sq;
 };
 
 template <int n, int d>
@@ -116,27 +105,6 @@ struct F0_fac: public cost_func<F0_fac<n, d>, n, d>
   void set_args(double const u[d + 1], double s_hat,
                 double const s[d + 1], double const p[d + 1][n],
                 double const p_fac[n], double s_fac);
-EIKONAL_PROTECTED:
-  double _sh;
-  double _u0;
-  double _du[d];
-  double _u_lam;
-  double _l;
-  double _q;
-  double _p_lam[n];
-  double _p0[n];
-  double _dP[d][n];
-  double _h;
-  double _theta;
-  double _p_fac[n];
-  double _s_fac;
-  double _tau0;
-  double _dtau[d];
-  double _tau_lam;
-  double _dPt_dot_p_fac[d];
-  double _p_lam_minus_p_fac[n];
-  double _l_fac_lam;
-  double _l_fac_lam_sq;
 };  
 
 template <int n, int d>
@@ -150,30 +118,6 @@ struct F1_fac: public cost_func<F1_fac<n, d>, n, d>
   void set_args(double const u[d + 1], double s_hat,
                 double const s[d + 1], double const p[d + 1][n],
                 double const p_fac[n], double s_fac);
-EIKONAL_PROTECTED:
-  double _s_hat;
-  double _stheta;
-  double _s[d + 1];
-  double _ds[d];
-  double _q;
-  double _l;
-  double _u_lam;
-  double _u0;
-  double _du[d];
-  double _p_lam[n];
-  double _p0[n];
-  double _dP[d][n];
-  double _h;
-  double _theta;
-  double _p_fac[n];
-  double _s_fac;
-  double _tau0;
-  double _dtau[d];
-  double _tau_lam;
-  double _dPt_dot_p_fac[d];
-  double _p_lam_minus_p_fac[n];
-  double _l_fac_lam;
-  double _l_fac_lam_sq;
 };
 
 template <class derived, char p0, char p1, char p2, int d>
@@ -202,28 +146,17 @@ struct cost_func_bv {
   }
 };
 
-template <char p0, char p1, char p2, int d>
-struct F0_bv: public cost_func_bv<F0_bv<p0, p1, p2, d>, p0, p1, p2, d> {
-  F0_bv(double h, double theta): _h {h}, _theta {theta} {}
-  void eval_impl(double & f) const;
-  void grad_impl(double df[d]) const;
-  void hess_impl(double d2f[__sym_mat_size(d)]) const;
-  void set_lambda_impl(double const lambda[d]);
-  void set_args_impl(double const u[d + 1], double s_hat,
-                     double const s[d + 1]);
-  // TODO: can probably delete this
-EIKONAL_PROTECTED:
-  double _sh;
-  double _u0;
-  double _du[d];
-  double _u_lam;
-  double _l;
-  double _y[d];
-  double _h;
-  double _theta;
-};
+// template <char p0, char p1, char p2, int d>
+// struct F0_bv: public cost_func_bv<F0_bv<p0, p1, p2, d>, p0, p1, p2, d> {
+//   F0_bv(double h, double theta): _h {h}, _theta {theta} {}
+//   void eval_impl(double & f) const;
+//   void grad_impl(double df[d]) const;
+//   void hess_impl(double d2f[__sym_mat_size(d)]) const;
+//   void set_lambda_impl(double const lambda[d]);
+//   void set_args_impl(double const u[d + 1], double s_hat,
+//                      double const s[d + 1]);
+// };
 
-// Specialization for d = 2
 template <char p0, char p1, char p2>
 struct F0_bv<p0, p1, p2, 2>:
   public cost_func_bv<F0_bv<p0, p1, p2, 2>, p0, p1, p2, 2>
@@ -235,39 +168,19 @@ struct F0_bv<p0, p1, p2, 2>:
   void set_lambda_impl(double const lambda[2]);
   void set_args_impl(double const u[3], double s_hat,
                      double const s[3]);
-EIKONAL_PROTECTED:
-  double _sh;
-  double _u0;
-  double _du[2];
-  double _u_lam;
-  double _l;
-  double _y[2];
-  double _h;
-  double _theta;
 };
 
-template <char p0, char p1, char p2, int d>
-struct F1_bv: public cost_func_bv<F1_bv<p0, p1, p2, d>, p0, p1, p2, d> {
-  F1_bv(double h, double theta): _h {h}, _theta {theta} {}
-  void eval_impl(double & f) const;
-  void grad_impl(double df[d]) const;
-  void hess_impl(double d2f[__sym_mat_size(d)]) const;
-  void set_lambda_impl(double const lambda[d]);
-  void set_args_impl(double const u[d + 1], double s_hat,
-                     double const s[d + 1]);
-EIKONAL_PROTECTED:
-  double _s_hat;
-  double _sh;
-  double _s0;
-  double _ds[d];
-  double _l;
-  double _u_lam;
-  double _u0;
-  double _du[d];
-  double _y[d];
-  double _h;
-  double _theta;
-};
+// template <char p0, char p1, char p2, int d>
+// struct F1_bv: public cost_func_bv<F1_bv<p0, p1, p2, d>, p0, p1, p2, d> {
+//   F1_bv(double h, double theta): _h {h}, _theta {theta} {}
+//   void eval_impl(double & f) const;
+//   void grad_impl(double df[d]) const;
+//   void hess_impl(double d2f[__sym_mat_size(d)]) const;
+//   void set_lambda_impl(double const lambda[d]);
+//   void set_args_impl(double const u[d + 1], double s_hat,
+//                      double const s[d + 1]);
+// EIKONAL_PROTECTED:
+// };
 
 // Specialization for d = 2
 template <char p0, char p1, char p2>

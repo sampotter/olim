1d57dd8f5cf0070e9585aa385a93fb33e26093e2
Sam Potter <sfp@hush.ai>
2018-09-07 18:14:20 -0400

    shuffling around lagrange multiplier stuff

--- src/cost_funcs.cpp
+++ src/cost_funcs.cpp
@@ -3,6 +3,92 @@
 #include <cassert>
 #include <cmath>
 
+#define __invert2x2inplace(X) do {              \
+    double const det = X[0]*X[2] - X[1]*X[1];   \
+    std::swap(X[0], X[2]);                      \
+    X[0] /= det;                                \
+    X[1] /= -det;                               \
+    X[2] /= det;                                \
+  } while (0)
+
+template <>
+void lagmults<2>(double const * lam, double const * df, double const * d2f,
+                 double * mu, int * k)
+{
+  // First, find the active set for lam
+
+  int active_set[2] = {-1, -1};
+  int num_active = 0;
+
+  if (fabs(lam[1]) < EPS(double)) active_set[num_active++] = 1;
+  if (fabs(lam[0]) < EPS(double)) active_set[num_active++] = 0;
+  if (fabs(1 - lam[0] - lam[1]) < EPS(double)) active_set[num_active++] = 2;
+
+  *k = num_active;
+
+  // If the active_set is {0, 1}, then mu = -df, which means that we
+  // can return early.
+
+  if (num_active == 2 &&
+      ((active_set[0] == 0 && active_set[1] == 1) ||
+       (active_set[0] == 1 && active_set[1] == 0))) {
+    mu[0] = -df[0];
+    mu[1] = -df[1];
+    goto coda;
+  } else {
+    __invert2x2inplace(d2f);
+    double p[2];
+    p[0] = d2f[0]*df[0] + d2f[1]*df[1];
+    p[1] = d2f[1]*df[0] + d2f[2]*df[1];
+
+    if (num_active == 1) {
+      if (active_set[0] == 0) {
+        mu[0] = -p[0]/d2f[0];
+      } else if (active_set[0] == 1) {
+        mu[0] = -p[1]/d2f[2];
+      } else if (active_set[0] == 2) {
+        mu[0] = (p[0] + p[1])/(d2f[0] + 2*d2f[1] + d2f[2]);
+      } else {
+        assert(false);
+        mu[0] = mu[1] = INF(double);
+      }
+    } else if (num_active == 2) {
+      // TODO: we can do this in place without defining A and b
+      double A[3], b[2];
+      if ((active_set[0] == 0 && active_set[1] == 2) ||
+          (active_set[0] == 2 && active_set[1] == 0)) {
+        A[0] = d2f[0];
+        A[1] = -(d2f[0] + d2f[1]);
+        A[2] = d2f[0] + 2*d2f[1] + d2f[2];
+        __invert2x2inplace(A);
+        b[0] = -p[0];
+        b[1] = p[0] + p[1];
+      } else if ((active_set[0] == 1 && active_set[1] == 2) ||
+                 (active_set[0] == 2 && active_set[1] == 1)) {
+        A[0] = d2f[2];
+        A[1] = -(d2f[1] + d2f[2]);
+        A[2] = d2f[0] + 2*d2f[1] + d2f[2];
+        __invert2x2inplace(A);
+        b[0] = -p[1];
+        b[1] = p[0] + p[1];
+      } else {
+        assert(false);
+        A[0] = A[1] = A[2] = b[0] = b[1] = INF(double);
+      }
+      mu[0] = A[0]*b[0] + A[1]*b[1];
+      mu[1] = A[1]*b[0] + A[2]*b[1];
+    } else {
+      assert(false);
+      mu[0] = mu[1] = INF(double);
+    }
+  }
+
+  __check(mu[0]);
+  __check(mu[1]);
+}
+
+#undef __invert2x2inplace
+
 template <>
 void
 F0<3, 2>::eval_impl(double & f) const
--- src/cost_funcs.impl.hpp
+++ src/cost_funcs.impl.hpp
@@ -7,126 +7,9 @@
 #include "common.macros.hpp"
 #include "update_rules.utils.hpp"
 
-#define __check(x) do {                         \
-    assert(!std::isinf(x));                     \
-    assert(!std::isnan(x));                     \
-  } while (0)
 
-#define __invert2x2inplace(X) do {              \
-    double const det = X[0]*X[2] - X[1]*X[1];   \
-    std::swap(X[0], X[2]);                      \
-    X[0] /= det;                                \
-    X[1] /= -det;                               \
-    X[2] /= det;                                \
-  } while (0)
-
-template <int d>
-inline void lag_mult_impl(double df[2], double d2f[3], double const lam[2],
-                          double * mu, int * k) {
-  static_assert(d == 2, "Haven't implemented lag_mult_impl for d != 2");
-
-  // First, find the active set for lam
-
-  int active_set[2] = {-1, -1};
-  int num_active = 0;
-
-  if (fabs(lam[1]) < EPS(double)) active_set[num_active++] = 1;
-  if (fabs(lam[0]) < EPS(double)) active_set[num_active++] = 0;
-  if (fabs(1 - lam[0] - lam[1]) < EPS(double)) active_set[num_active++] = 2;
-
-  *k = num_active;
-
-  // If the active_set is {0, 1}, then mu = -df, which means that we
-  // can return early.
-
-  if (num_active == 2 &&
-      ((active_set[0] == 0 && active_set[1] == 1) ||
-       (active_set[0] == 1 && active_set[1] == 0))) {
-    mu[0] = -df[0];
-    mu[1] = -df[1];
-    return;
-  }
-
-  __invert2x2inplace(d2f);
-  double p[2];
-  p[0] = d2f[0]*df[0] + d2f[1]*df[1];
-  p[1] = d2f[1]*df[0] + d2f[2]*df[1];
-
-  if (num_active == 1) {
-    if (active_set[0] == 0) {
-      mu[0] = -p[0]/d2f[0];
-    } else if (active_set[0] == 1) {
-      mu[0] = -p[1]/d2f[2];
-    } else if (active_set[0] == 2) {
-      mu[0] = (p[0] + p[1])/(d2f[0] + 2*d2f[1] + d2f[2]);
-    } else {
-      assert(false);
-      mu[0] = mu[1] = INF(double);
-    }
-  } else if (num_active == 2) {
-    // TODO: we can do this in place without defining A and b
-    double A[3], b[2];
-    if ((active_set[0] == 0 && active_set[1] == 2) ||
-        (active_set[0] == 2 && active_set[1] == 0)) {
-      A[0] = d2f[0];
-      A[1] = -(d2f[0] + d2f[1]);
-      A[2] = d2f[0] + 2*d2f[1] + d2f[2];
-      __invert2x2inplace(A);
-      b[0] = -p[0];
-      b[1] = p[0] + p[1];
-    } else if ((active_set[0] == 1 && active_set[1] == 2) ||
-               (active_set[0] == 2 && active_set[1] == 1)) {
-      A[0] = d2f[2];
-      A[1] = -(d2f[1] + d2f[2]);
-      A[2] = d2f[0] + 2*d2f[1] + d2f[2];
-      __invert2x2inplace(A);
-      b[0] = -p[1];
-      b[1] = p[0] + p[1];
-    } else {
-      assert(false);
-      A[0] = A[1] = A[2] = b[0] = b[1] = INF(double);
-    }
-    mu[0] = A[0]*b[0] + A[1]*b[1];
-    mu[1] = A[1]*b[0] + A[2]*b[1];
-  } else {
-    assert(false);
-    mu[0] = mu[1] = INF(double);
-  }
-}
-
-#undef __invert2x2inplace
-
-template <class derived, int n, int d>
-void
-cost_func<derived, n, d>::lag_mult(double const lam[d], double * mu, int * k)
-{
-  static_assert(n == 3 && d == 2,
-                "Haven't implemented lag_mult for n != 3 or d != 2");
-  double df[2], d2f[3];
-  set_lambda(lam);
-  grad(df);
-  hess(d2f);
-  lag_mult_impl<d>(df, d2f, lam, mu, k);
-  for (int i = 0; i < *k; ++i) {
-    __check(mu[i]);
-  }
-}
-
-template <class derived, char p0, char p1, char p2, int d>
-void
-cost_func_bv<derived, p0, p1, p2, d>::lag_mult(double const lam[d], double * mu,
-                                               int * k)
-{
-  static_assert(d == 2, "Haven't implemented lag_mult for d != 2");
-  double df[2], d2f[3];
-  set_lambda(lam);
-  grad(df);
-  hess(d2f);
-  lag_mult_impl<d>(df, d2f, lam, mu, k);
-  for (int i = 0; i < *k; ++i) {
-    __check(mu[i]);
-  }
-}
+////////////////////////////////////////////////////////////////////////////////
+// TODO: consolidate this stuff into a "bit ops" module ////////////////////////
 
 template <char pj, char p0>
 constexpr char dp(char i) {
@@ -139,6 +22,22 @@ constexpr char dp(char i) {
     dp<p##i, p0>(0)*dp<p##j, p0>(0) +           \
     dp<p##i, p0>(1)*dp<p##j, p0>(1) +           \
     dp<p##i, p0>(2)*dp<p##j, p0>(2))
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// TODO: move this into a common "num utils" module ////////////////////////////
+
+#define __check(x) do {                         \
+    assert(!std::isinf(x));                     \
+    assert(!std::isnan(x));                     \
+  } while (0)
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 
 template <char p0, char p1, char p2>
 void
@@ -251,7 +150,6 @@ F1_bv<p0, p1, p2, 2>::set_lambda_impl(double const lam[2])
   _y[1] = (__dP(2, 0)*p[0] + __dP(2, 1)*p[1] + __dP(2, 2)*p[2])/_l;
 }
 
-#undef __check
 #undef __dP
 
 #endif // __COST_FUNCS_IMPL_HPP__

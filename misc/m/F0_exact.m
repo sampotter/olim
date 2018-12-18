9756f6ce53fae2a088389e770e47413662eaef9e
Sam Potter <sfp@hush.ai>
2018-12-17 20:19:23 -0500

    update F0_exact.m

--- misc/m/F0_exact.m
+++ misc/m/F0_exact.m
@@ -1,12 +1,25 @@
 function [u, lam, deg] = F0_exact(u0, du, p0, dP, sh)
+    deg = false;
     [Q, R] = qr(dP, 0);
     I = eye(size(Q, 1));
     q = inv(R')*du/sh;
     if dot(q, q) >= 1
         deg = true;
-        return;
+        u = inf;
+        lam = [nan nan];
+    else
+        lopt = sqrt(p0'*(I - Q*Q')*p0/(1 - dot(q, q)));
+        lam = -inv(R)*(Q'*p0 + lopt*q);
+        if ~in_simplex(lam)
+            deg = true;
+            u = inf;
+        else
+            u0 + du'*lam + sh*lopt
+            u = u0 + sh*p0'*(p0 + dP*lam)/lopt;
+        end
     end
-    lopt = sqrt(p0'*(I - Q*Q')*p0/(1 - dot(q, q)));
-    lam = -inv(R)*(Q'*p0 + lopt*q);
-    u = u0 + sh*p0'*(p0 + dP*lam)/lopt;
+end
+
+function [tf] = in_simplex(lam)
+    tf = all(lam >= 0) & sum(lam) <= 1;
 end

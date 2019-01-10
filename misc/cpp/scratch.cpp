f934344c6966313027556e26b12ff319ff819acb
Sam Potter <sfp@hush.ai>
2019-01-10 14:32:44 -0500

    update scratch.cpp

--- misc/cpp/scratch.cpp
+++ misc/cpp/scratch.cpp
@@ -1,9 +1,10 @@
+#include <basic_marcher_3d.hpp>
 #include <olim.hpp>
 #include <olim3d.hpp>
 #include <src/config.hpp>
 #include <string>
 
-constexpr int n = 33;
+constexpr int n = 65;
 
 int main() {
 
@@ -11,22 +12,24 @@ int main() {
   double h = 2./(n-1);
 
   double * S = new double[n*n*n];
-  // for (int i = 0; i < n*n*n; ++i) S[i] = 1;
-  double x, y, z;
-  for (int i = 0; i < n; ++i) {
-    x = h*static_cast<double>(i) - 1;
-    for (int j = 0; j < n; ++j) {
-      y = h*static_cast<double>(j) - 1;
-      for (int k = 0; k < n; ++k) {
-        z = h*static_cast<double>(k) - 1;
-        S[n*(n*k + j) + i] = s2(x, y, z);
-      }
-    }
-  }
+  for (int i = 0; i < n*n*n; ++i) S[i] = 1;
+  // double x, y, z;
+  // for (int i = 0; i < n; ++i) {
+  //   x = h*static_cast<double>(i) - 1;
+  //   for (int j = 0; j < n; ++j) {
+  //     y = h*static_cast<double>(j) - 1;
+  //     for (int k = 0; k < n; ++k) {
+  //       z = h*static_cast<double>(k) - 1;
+  //       S[n*(n*k + j) + i] = s2(x, y, z);
+  //     }
+  //   }
+  // }
   
   // olim18_mp1 o {n, n, n, h, S};
   // olim26_mp0 o {n, n, n, h, S};
-  olim3d_hu_mp0 o {n, n, n, h, S};
+  // olim3d_hu_mp0 o {n, n, n, h, S};
+  // basic_marcher_3d o {n, n, n, h, S};
+  olim6_rhr o {n, n, n, h, S};
 
   // for (int i = 0; i < n; ++i) {
   //   for (int j = 0; j < n; ++j) {

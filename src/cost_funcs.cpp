71de8633adb290b43856bed8faf0a3d2161d7333
Sam Potter <sfp@hush.ai>
2019-01-13 16:18:44 -0500

    finish optimizing `lagmults'

--- src/cost_funcs.cpp
+++ src/cost_funcs.cpp
@@ -4,24 +4,26 @@
 
 template <>
 void lagmults<2>(double const * lam, double const * df, double const * d2f,
-                 double * mu, int * k)
+                 double * mu, int * k_ptr)
 {
   check_lambda<2>(lam);
 
   int I[2] = {-1, -1};
 
-  // First, find the active set, I, for lam
-  *k = 0;
-  if (fabs(lam[0]) < eps<double>) I[(*k)++] = 0;
-  if (fabs(lam[1]) < eps<double>) I[(*k)++] = 1;
-  if (fabs(1 - lam[0] - lam[1]) < eps<double>) I[(*k)++] = 2;
+  int & k = *k_ptr;
 
-  if (*k == 2 && I[0] != 2 && I[1] != 2) {
+  // First, find the active set, I, for lam
+  k = 0;
+  if (fabs(lam[0]) < eps<double>) I[k++] = 0;
+  if (fabs(lam[1]) < eps<double>) I[k++] = 1;
+  if (fabs(1 - lam[0] - lam[1]) < eps<double>) I[k++] = 2;
+
+  if (k == 2 && I[0] != 2 && I[1] != 2) {
     // If I = {0, 1}, then mu = df. Simple enough.
     mu[0] = df[0];
     mu[1] = df[1];
   } else {
-    if (*k == 1) {
+    if (k == 1) {
       // When k = 1, things simplify a lot:
       if (I[0] == 0) {
         mu[0] = df[0] - d2f[1]*df[1]/d2f[2];
@@ -33,28 +35,20 @@ void lagmults<2>(double const * lam, double const * df, double const * d2f,
       } else {
         assert(false);
       }
-    } else if (*k == 2) {
-      // TODO: finish optimizing this case
-      double det, inv[3], p[2];
-      det = d2f[0]*d2f[2] - d2f[1]*d2f[1];
-      if (fabs(det) < eps<double>) {
+    } else if (k == 2) {
+      if (fabs(d2f[0]*d2f[2] - d2f[1]*d2f[1]) < eps<double>) {
         mu[0] = mu[1] = 0;
       } else {
-        inv[0] = d2f[2]/det;
-        inv[1] = -d2f[1]/det;
-        inv[2] = d2f[0]/det;
-        p[0] = inv[0]*df[0] + inv[1]*df[1];
-        p[1] = inv[1]*df[0] + inv[2]*df[1];
         if (I[0] != 1 && I[1] != 1) {
           // When I = {0, 2}, A = [1 0; -1 -1] = inv(A), which we use to
           // compute the following matrix-vector product.
-          mu[0] = (d2f[0] - 2*d2f[1] + d2f[2])*p[0] + (d2f[1] - d2f[2])*(p[0] + p[1]);
-          mu[1] = (d2f[2] - d2f[1])*p[0] - d2f[2]*(p[0] + p[1]);
+          mu[0] = df[0] - df[1];
+          mu[1] = -df[1];
         } else if (I[0] != 0 && I[1] != 0) {
           // Likewise, when I = {1, 2}, we have A = [0 1; -1 -1] and
           // inv(A) = [-1 -1; 1 0], which we use to compute mu.
-          mu[0] = (d2f[0] - 2*d2f[1] + d2f[2])*p[1] - (d2f[0] - d2f[1])*(p[0] + p[1]);
-          mu[1] = (d2f[0] - d2f[1])*p[1] - d2f[0]*(p[0] + p[1]);
+          mu[0] = df[1] - df[0];
+          mu[1] = -df[0];
         } else {
           assert(false);
         }

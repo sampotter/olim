1319499f7c95fda30267b3690f4fe3e0c9d71c07
Sam Potter <sfp@hush.ai>
2019-01-30 10:59:39 -0500

    remove this unused enum

--- src/typedefs.h
+++ src/typedefs.h
@@ -1,23 +1,3 @@
 #pragma once
 
 enum class state: char {valid, trial, far};
-
-enum marcher_type {
-  BASIC,
-  OLIM4_MP0,
-  OLIM4_RHR,
-  OLIM4_RHR_LUT,
-  OLIM6_MP0,
-  OLIM6_MP1,
-  OLIM6_RHR,
-  OLIM8_MP0,
-  OLIM8_MP1,
-  OLIM8_RHR,
-  OLIM18_MP0,
-  OLIM18_MP1,
-  OLIM18_RHR,
-  OLIM26_MP0,
-  OLIM26_MP1,
-  OLIM26_RHR,
-  SOLIM4_MP0
-};

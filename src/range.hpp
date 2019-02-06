69db42f054ef01e2a668d8b2db112a0ab54233f1
Sam Potter <sfp@hush.ai>
2019-02-06 09:48:47 -0500

    add `ord' template parameter to range for column/row-major ordering

--- src/range.hpp
+++ src/range.hpp
@@ -3,40 +3,40 @@
 #include "common.hpp"
 #include "vec.hpp"
 
-template <int n>
+template <int n, ordering ord = ordering::COLUMN_MAJOR>
 struct range
 {
   range(vec<int, n> dims):
     _dims {dims},
     _end_lin {_dims.product()},
-    _end {to_vector_index(_end_lin, _dims)}
+    _end {to_vector_index<ord>(_end_lin, _dims)}
   {}
 
-  inline range<n> & operator++() {
+  inline range<n, ord> & operator++() {
     ++_lin;
     return *this;
   }
 
-  inline range<n> operator++(int) {
-    range<n> tmp {*this};
+  inline range<n, ord> operator++(int) {
+    range<n, ord> tmp {*this};
     ++(*this);
     return tmp;
   }
 
-  inline bool operator!=(range<n> const & other) const {
+  inline bool operator!=(range<n, ord> const & other) const {
     return _lin != other._lin || _dims != other._dims;
   }
 
   inline vec<int, n> operator*() const {
-    return to_vector_index(_lin, _dims);
+    return to_vector_index<ord>(_lin, _dims);
   }
 
-  inline range<n> begin() const {
+  inline range<n, ord> begin() const {
     return {_dims};
   }
 
-  inline range<n> end() const {
-    range<n> tmp {_dims};
+  inline range<n, ord> end() const {
+    range<n, ord> tmp {_dims};
     tmp._lin = tmp._end_lin;
     return tmp;
   }

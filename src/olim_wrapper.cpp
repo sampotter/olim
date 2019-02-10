268d0cc41650dac768818a4dd9ca8a01b803d52a
Sam Potter <sfp@hush.ai>
2019-02-10 18:26:07 -0500

    get the wrapper and Cython binding working

--- src/olim_wrapper.cpp
+++ src/olim_wrapper.cpp
@@ -1,66 +1,542 @@
 #include "olim_wrapper.h"
 
-#include <string>
-#include <variant>
-
 #include "olim.hpp"
 #include "olim3d.hpp"
 
-struct olim_wrapper {
-  olim4_rhr * olim;
+// TODO: this is pretty awful, but at least it works. Definitely need
+// to improve on this somehow. I tried std::variant, but ran into
+// problems with it. Not totally sure what the cause of them was.
+
+struct null_olim {
+  void run() {}
+  void add_boundary_node(int *, double) {}
+  double * get_U_ptr() const { return nullptr; }
+  double * get_s_ptr() const { return nullptr; }
+  char * get_state_ptr() const { return nullptr; }
 };
 
-status_e olim_wrapper_init(olim_wrapper_s ** w_ptr, olim_wrapper_params_s * p)
-{
-  olim_wrapper_s * w = (*w_ptr = new olim_wrapper_s);
+union olim_variant {
+  ~olim_variant() {}
+  null_olim _null_olim;
+  olim4_mp0 _olim4_mp0;
+  olim4_mp1 _olim4_mp1;
+  olim4_rhr _olim4_rhr;
+  olim8_mp0 _olim8_mp0;
+  olim8_mp1 _olim8_mp1;
+  olim8_rhr _olim8_rhr;
+  olim6_mp0 _olim6_mp0;
+  olim6_mp1 _olim6_mp1;
+  olim6_rhr _olim6_rhr;
+  olim18_mp0 _olim18_mp0;
+  olim18_mp1 _olim18_mp1;
+  olim18_rhr _olim18_rhr;
+  olim26_mp0 _olim26_mp0;
+  olim26_mp1 _olim26_mp1;
+  olim26_rhr _olim26_rhr;
+  olim3d_hu_mp0 _olim3d_hu_mp0;
+  olim3d_hu_mp1 _olim3d_hu_mp1;
+  olim3d_hu_rhr _olim3d_hu_rhr;
+};
 
-  w->olim = new olim4_rhr {{p->dims}, p->h, no_slow_t {}};
+struct olim_wrapper
+{
+  neighborhood nb;
+  cost_func F;
+  olim_variant _olim;
+
+  template <class olim_t>
+  olim_t & olim();
+
+  template <>
+  olim4_mp0 & olim<olim4_mp0>() {
+    return _olim._olim4_mp0;
+  }
+
+  template <>
+  olim4_mp1 & olim<olim4_mp1>() {
+    return _olim._olim4_mp1;
+  }
+
+  template <>
+  olim4_rhr & olim<olim4_rhr>() {
+    return _olim._olim4_rhr;
+  }
+
+  template <>
+  olim8_mp0 & olim<olim8_mp0>() {
+    return _olim._olim8_mp0;
+  }
+
+  template <>
+  olim8_mp1 & olim<olim8_mp1>() {
+    return _olim._olim8_mp1;
+  }
+
+  template <>
+  olim8_rhr & olim<olim8_rhr>() {
+    return _olim._olim8_rhr;
+  }
+
+  template <>
+  olim6_mp0 & olim<olim6_mp0>() {
+    return _olim._olim6_mp0;
+  }
+
+  template <>
+  olim6_mp1 & olim<olim6_mp1>() {
+    return _olim._olim6_mp1;
+  }
+
+  template <>
+  olim6_rhr & olim<olim6_rhr>() {
+    return _olim._olim6_rhr;
+  }
+
+  template <>
+  olim18_mp0 & olim<olim18_mp0>() {
+    return _olim._olim18_mp0;
+  }
+
+  template <>
+  olim18_mp1 & olim<olim18_mp1>() {
+    return _olim._olim18_mp1;
+  }
+
+  template <>
+  olim18_rhr & olim<olim18_rhr>() {
+    return _olim._olim18_rhr;
+  }
+
+  template <>
+  olim26_mp0 & olim<olim26_mp0>() {
+    return _olim._olim26_mp0;
+  }
+
+  template <>
+  olim26_mp1 & olim<olim26_mp1>() {
+    return _olim._olim26_mp1;
+  }
+
+  template <>
+  olim26_rhr & olim<olim26_rhr>() {
+    return _olim._olim26_rhr;
+  }
+
+  template <>
+  olim3d_hu_mp0 & olim<olim3d_hu_mp0>() {
+    return _olim._olim3d_hu_mp0;
+  }
+
+  template <>
+  olim3d_hu_mp1 & olim<olim3d_hu_mp1>() {
+    return _olim._olim3d_hu_mp1;
+  }
+
+  template <>
+  olim3d_hu_rhr & olim<olim3d_hu_rhr>() {
+    return _olim._olim3d_hu_rhr;
+  }
+};
+
+template <class olim_t>
+void construct_olim(olim_wrapper * w, olim_wrapper_params_s * p)
+{
+  new (&w->olim<olim_t>()) olim_t {{p->dims}, p->h, no_slow_t {}};
+}
+
+template <class olim_t>
+void destruct_olim(olim_wrapper * w)
+{
+  w->olim<olim_t>().~olim_t();
+}
+
+status_e olim_wrapper_init(olim_wrapper ** w_ptr, olim_wrapper_params_s * p)
+{
+  olim_wrapper * w = (*w_ptr = new olim_wrapper {p->nb, p->F, {}});
+
+  if (p->nb == OLIM4) {
+    if (p->F == MP0) {
+      construct_olim<olim4_mp0>(w, p);
+    } else if (p->F == MP1) {
+      construct_olim<olim4_mp1>(w, p);
+    } else if (p->F == RHR) {
+      construct_olim<olim4_rhr>(w, p);
+    }
+  } else if (p->nb == OLIM8) {
+    if (p->F == MP0) {
+      construct_olim<olim8_mp0>(w, p);
+    } else if (p->F == MP1) {
+      construct_olim<olim8_mp1>(w, p);
+    } else if (p->F == RHR) {
+      construct_olim<olim8_rhr>(w, p);
+    }
+  } else if (p->nb == OLIM6) {
+    if (p->F == MP0) {
+      construct_olim<olim6_mp0>(w, p);
+    } else if (p->F == MP1) {
+      construct_olim<olim6_mp1>(w, p);
+    } else if (p->F == RHR) {
+      construct_olim<olim6_rhr>(w, p);
+    }
+  } else if (p->nb == OLIM18) {
+    if (p->F == MP0) {
+      construct_olim<olim18_mp0>(w, p);
+    } else if (p->F == MP1) {
+      construct_olim<olim18_mp1>(w, p);
+    } else if (p->F == RHR) {
+      construct_olim<olim18_rhr>(w, p);
+    }
+  } else if (p->nb == OLIM26) {
+    if (p->F == MP0) {
+      construct_olim<olim26_mp0>(w, p);
+    } else if (p->F == MP1) {
+      construct_olim<olim26_mp1>(w, p);
+    } else if (p->F == RHR) {
+      construct_olim<olim26_rhr>(w, p);
+    }
+  } else if (p->nb == OLIM3D) {
+    if (p->F == MP0) {
+      construct_olim<olim3d_hu_mp0>(w, p);
+    } else if (p->F == MP1) {
+      construct_olim<olim3d_hu_mp1>(w, p);
+    } else if (p->F == RHR) {
+      construct_olim<olim3d_hu_rhr>(w, p);
+    }
+  }
 
   return SUCCESS;
 }
 
-status_e olim_wrapper_deinit(olim_wrapper_s ** w_ptr)
+status_e olim_wrapper_deinit(olim_wrapper ** w_ptr)
 {
-  olim_wrapper_s * w = *w_ptr;
-  delete w->olim;
-  
+  olim_wrapper * w = *w_ptr;
+
+  if (w->nb == OLIM4) {
+    if (w->F == MP0) {
+      destruct_olim<olim4_mp0>(w);
+    } else if (w->F == MP1) {
+      destruct_olim<olim4_mp1>(w);
+    } else if (w->F == RHR) {
+      destruct_olim<olim4_rhr>(w);
+    }
+  } else if (w->nb == OLIM8) {
+    if (w->F == MP0) {
+      destruct_olim<olim8_mp0>(w);
+    } else if (w->F == MP1) {
+      destruct_olim<olim8_mp1>(w);
+    } else if (w->F == RHR) {
+      destruct_olim<olim8_rhr>(w);
+    }
+  } else if (w->nb == OLIM6) {
+    if (w->F == MP0) {
+      destruct_olim<olim6_mp0>(w);
+    } else if (w->F == MP1) {
+      destruct_olim<olim6_mp1>(w);
+    } else if (w->F == RHR) {
+      destruct_olim<olim6_rhr>(w);
+    }
+  } else if (w->nb == OLIM18) {
+    if (w->F == MP0) {
+      destruct_olim<olim18_mp0>(w);
+    } else if (w->F == MP1) {
+      destruct_olim<olim18_mp1>(w);
+    } else if (w->F == RHR) {
+      destruct_olim<olim18_rhr>(w);
+    }
+  } else if (w->nb == OLIM26) {
+    if (w->F == MP0) {
+      destruct_olim<olim26_mp0>(w);
+    } else if (w->F == MP1) {
+      destruct_olim<olim26_mp1>(w);
+    } else if (w->F == RHR) {
+      destruct_olim<olim26_rhr>(w);
+    }
+  } else if (w->nb == OLIM3D) {
+    if (w->F == MP0) {
+      destruct_olim<olim3d_hu_mp0>(w);
+    } else if (w->F == MP1) {
+      destruct_olim<olim3d_hu_mp1>(w);
+    } else if (w->F == RHR) {
+      destruct_olim<olim3d_hu_rhr>(w);
+    }
+  }
+
   delete *w_ptr;
 
   return SUCCESS;
 }
 
-status_e olim_wrapper_run(olim_wrapper_s * w)
+status_e olim_wrapper_run(olim_wrapper * w)
 {
-  w->olim->run();
+  if (w->nb == OLIM4) {
+    if (w->F == MP0) {
+      w->olim<olim4_mp0>().run();
+    } else if (w->F == MP1) {
+      w->olim<olim4_mp1>().run();
+    } else if (w->F == RHR) {
+      w->olim<olim4_rhr>().run();
+    }
+  } else if (w->nb == OLIM8) {
+    if (w->F == MP0) {
+      w->olim<olim8_mp0>().run();
+    } else if (w->F == MP1) {
+      w->olim<olim8_mp1>().run();
+    } else if (w->F == RHR) {
+      w->olim<olim8_rhr>().run();
+    }
+  } else if (w->nb == OLIM6) {
+    if (w->F == MP0) {
+      w->olim<olim6_mp0>().run();
+    } else if (w->F == MP1) {
+      w->olim<olim6_mp1>().run();
+    } else if (w->F == RHR) {
+      w->olim<olim6_rhr>().run();
+    }
+  } else if (w->nb == OLIM18) {
+    if (w->F == MP0) {
+      w->olim<olim18_mp0>().run();
+    } else if (w->F == MP1) {
+      w->olim<olim18_mp1>().run();
+    } else if (w->F == RHR) {
+      w->olim<olim18_rhr>().run();
+    }
+  } else if (w->nb == OLIM26) {
+    if (w->F == MP0) {
+      w->olim<olim26_mp0>().run();
+    } else if (w->F == MP1) {
+      w->olim<olim26_mp1>().run();
+    } else if (w->F == RHR) {
+      w->olim<olim26_rhr>().run();
+    }
+  } else if (w->nb == OLIM3D) {
+    if (w->F == MP0) {
+      w->olim<olim3d_hu_mp0>().run();
+    } else if (w->F == MP1) {
+      w->olim<olim3d_hu_mp1>().run();
+    } else if (w->F == RHR) {
+      w->olim<olim3d_hu_rhr>().run();
+    }
+  }
 
   return SUCCESS;
 }
 
-status_e olim_wrapper_add_bd(olim_wrapper_s * w, int * inds, double U)
+status_e olim_wrapper_add_bd(olim_wrapper * w, int * inds, double U)
 {
-  w->olim->add_boundary_node(vec2<int> {inds}, U);
+  if (w->nb == OLIM4) {
+    if (w->F == MP0) {
+      w->olim<olim4_mp0>().add_boundary_node(inds, U);
+    } else if (w->F == MP1) {
+      w->olim<olim4_mp1>().add_boundary_node(inds, U);
+    } else if (w->F == RHR) {
+      w->olim<olim4_rhr>().add_boundary_node(inds, U);
+    }
+  } else if (w->nb == OLIM8) {
+    if (w->F == MP0) {
+      w->olim<olim8_mp0>().add_boundary_node(inds, U);
+    } else if (w->F == MP1) {
+      w->olim<olim8_mp1>().add_boundary_node(inds, U);
+    } else if (w->F == RHR) {
+      w->olim<olim8_rhr>().add_boundary_node(inds, U);
+    }
+  } else if (w->nb == OLIM6) {
+    if (w->F == MP0) {
+      w->olim<olim6_mp0>().add_boundary_node(inds, U);
+    } else if (w->F == MP1) {
+      w->olim<olim6_mp1>().add_boundary_node(inds, U);
+    } else if (w->F == RHR) {
+      w->olim<olim6_rhr>().add_boundary_node(inds, U);
+    }
+  } else if (w->nb == OLIM18) {
+    if (w->F == MP0) {
+      w->olim<olim18_mp0>().add_boundary_node(inds, U);
+    } else if (w->F == MP1) {
+      w->olim<olim18_mp1>().add_boundary_node(inds, U);
+    } else if (w->F == RHR) {
+      w->olim<olim18_rhr>().add_boundary_node(inds, U);
+    }
+  } else if (w->nb == OLIM26) {
+    if (w->F == MP0) {
+      w->olim<olim26_mp0>().add_boundary_node(inds, U);
+    } else if (w->F == MP1) {
+      w->olim<olim26_mp1>().add_boundary_node(inds, U);
+    } else if (w->F == RHR) {
+      w->olim<olim26_rhr>().add_boundary_node(inds, U);
+    }
+  } else if (w->nb == OLIM3D) {
+    if (w->F == MP0) {
+      w->olim<olim3d_hu_mp0>().add_boundary_node(inds, U);
+    } else if (w->F == MP1) {
+      w->olim<olim3d_hu_mp1>().add_boundary_node(inds, U);
+    } else if (w->F == RHR) {
+      w->olim<olim3d_hu_rhr>().add_boundary_node(inds, U);
+    }
+  }
 
   return SUCCESS;
 }
 
-status_e olim_wrapper_get_U_ptr(olim_wrapper_s * w, double ** U_ptr)
+status_e olim_wrapper_get_U_ptr(olim_wrapper * w, double ** U_ptr)
 {
-  *U_ptr = w->olim->get_U_ptr();
+  if (w->nb == OLIM4) {
+    if (w->F == MP0) {
+      *U_ptr = w->olim<olim4_mp0>().get_U_ptr();
+    } else if (w->F == MP1) {
+      *U_ptr = w->olim<olim4_mp1>().get_U_ptr();
+    } else if (w->F == RHR) {
+      *U_ptr = w->olim<olim4_rhr>().get_U_ptr();
+    }
+  } else if (w->nb == OLIM8) {
+    if (w->F == MP0) {
+      *U_ptr = w->olim<olim8_mp0>().get_U_ptr();
+    } else if (w->F == MP1) {
+      *U_ptr = w->olim<olim8_mp1>().get_U_ptr();
+    } else if (w->F == RHR) {
+      *U_ptr = w->olim<olim8_rhr>().get_U_ptr();
+    }
+  } else if (w->nb == OLIM6) {
+    if (w->F == MP0) {
+      *U_ptr = w->olim<olim6_mp0>().get_U_ptr();
+    } else if (w->F == MP1) {
+      *U_ptr = w->olim<olim6_mp1>().get_U_ptr();
+    } else if (w->F == RHR) {
+      *U_ptr = w->olim<olim6_rhr>().get_U_ptr();
+    }
+  } else if (w->nb == OLIM18) {
+    if (w->F == MP0) {
+      *U_ptr = w->olim<olim18_mp0>().get_U_ptr();
+    } else if (w->F == MP1) {
+      *U_ptr = w->olim<olim18_mp1>().get_U_ptr();
+    } else if (w->F == RHR) {
+      *U_ptr = w->olim<olim18_rhr>().get_U_ptr();
+    }
+  } else if (w->nb == OLIM26) {
+    if (w->F == MP0) {
+      *U_ptr = w->olim<olim26_mp0>().get_U_ptr();
+    } else if (w->F == MP1) {
+      *U_ptr = w->olim<olim26_mp1>().get_U_ptr();
+    } else if (w->F == RHR) {
+      *U_ptr = w->olim<olim26_rhr>().get_U_ptr();
+    }
+  } else if (w->nb == OLIM3D) {
+    if (w->F == MP0) {
+      *U_ptr = w->olim<olim3d_hu_mp0>().get_U_ptr();
+    } else if (w->F == MP1) {
+      *U_ptr = w->olim<olim3d_hu_mp1>().get_U_ptr();
+    } else if (w->F == RHR) {
+      *U_ptr = w->olim<olim3d_hu_rhr>().get_U_ptr();
+    }
+  }
 
   return SUCCESS;
 }
 
-status_e olim_wrapper_get_s_ptr(olim_wrapper_s * w, double ** s_ptr)
+status_e olim_wrapper_get_s_ptr(olim_wrapper * w, double ** s_ptr)
 {
-  *s_ptr = w->olim->get_s_ptr();
+  if (w->nb == OLIM4) {
+    if (w->F == MP0) {
+      *s_ptr = w->olim<olim4_mp0>().get_s_ptr();
+    } else if (w->F == MP1) {
+      *s_ptr = w->olim<olim4_mp1>().get_s_ptr();
+    } else if (w->F == RHR) {
+      *s_ptr = w->olim<olim4_rhr>().get_s_ptr();
+    }
+  } else if (w->nb == OLIM8) {
+    if (w->F == MP0) {
+      *s_ptr = w->olim<olim8_mp0>().get_s_ptr();
+    } else if (w->F == MP1) {
+      *s_ptr = w->olim<olim8_mp1>().get_s_ptr();
+    } else if (w->F == RHR) {
+      *s_ptr = w->olim<olim8_rhr>().get_s_ptr();
+    }
+  } else if (w->nb == OLIM6) {
+    if (w->F == MP0) {
+      *s_ptr = w->olim<olim6_mp0>().get_s_ptr();
+    } else if (w->F == MP1) {
+      *s_ptr = w->olim<olim6_mp1>().get_s_ptr();
+    } else if (w->F == RHR) {
+      *s_ptr = w->olim<olim6_rhr>().get_s_ptr();
+    }
+  } else if (w->nb == OLIM18) {
+    if (w->F == MP0) {
+      *s_ptr = w->olim<olim18_mp0>().get_s_ptr();
+    } else if (w->F == MP1) {
+      *s_ptr = w->olim<olim18_mp1>().get_s_ptr();
+    } else if (w->F == RHR) {
+      *s_ptr = w->olim<olim18_rhr>().get_s_ptr();
+    }
+  } else if (w->nb == OLIM26) {
+    if (w->F == MP0) {
+      *s_ptr = w->olim<olim26_mp0>().get_s_ptr();
+    } else if (w->F == MP1) {
+      *s_ptr = w->olim<olim26_mp1>().get_s_ptr();
+    } else if (w->F == RHR) {
+      *s_ptr = w->olim<olim26_rhr>().get_s_ptr();
+    }
+  } else if (w->nb == OLIM3D) {
+    if (w->F == MP0) {
+      *s_ptr = w->olim<olim3d_hu_mp0>().get_s_ptr();
+    } else if (w->F == MP1) {
+      *s_ptr = w->olim<olim3d_hu_mp1>().get_s_ptr();
+    } else if (w->F == RHR) {
+      *s_ptr = w->olim<olim3d_hu_rhr>().get_s_ptr();
+    }
+  }
 
   return SUCCESS;
 }
 
-
-status_e olim_wrapper_get_state_ptr(olim_wrapper_s * w, char ** state_ptr)
+status_e olim_wrapper_get_state_ptr(olim_wrapper * w, char ** state_ptr)
 {
-  *state_ptr = w->olim->get_state_ptr();
+  if (w->nb == OLIM4) {
+    if (w->F == MP0) {
+      *state_ptr = w->olim<olim4_mp0>().get_state_ptr();
+    } else if (w->F == MP1) {
+      *state_ptr = w->olim<olim4_mp1>().get_state_ptr();
+    } else if (w->F == RHR) {
+      *state_ptr = w->olim<olim4_rhr>().get_state_ptr();
+    }
+  } else if (w->nb == OLIM8) {
+    if (w->F == MP0) {
+      *state_ptr = w->olim<olim8_mp0>().get_state_ptr();
+    } else if (w->F == MP1) {
+      *state_ptr = w->olim<olim8_mp1>().get_state_ptr();
+    } else if (w->F == RHR) {
+      *state_ptr = w->olim<olim8_rhr>().get_state_ptr();
+    }
+  } else if (w->nb == OLIM6) {
+    if (w->F == MP0) {
+      *state_ptr = w->olim<olim6_mp0>().get_state_ptr();
+    } else if (w->F == MP1) {
+      *state_ptr = w->olim<olim6_mp1>().get_state_ptr();
+    } else if (w->F == RHR) {
+      *state_ptr = w->olim<olim6_rhr>().get_state_ptr();
+    }
+  } else if (w->nb == OLIM18) {
+    if (w->F == MP0) {
+      *state_ptr = w->olim<olim18_mp0>().get_state_ptr();
+    } else if (w->F == MP1) {
+      *state_ptr = w->olim<olim18_mp1>().get_state_ptr();
+    } else if (w->F == RHR) {
+      *state_ptr = w->olim<olim18_rhr>().get_state_ptr();
+    }
+  } else if (w->nb == OLIM26) {
+    if (w->F == MP0) {
+      *state_ptr = w->olim<olim26_mp0>().get_state_ptr();
+    } else if (w->F == MP1) {
+      *state_ptr = w->olim<olim26_mp1>().get_state_ptr();
+    } else if (w->F == RHR) {
+      *state_ptr = w->olim<olim26_rhr>().get_state_ptr();
+    }
+  } else if (w->nb == OLIM3D) {
+    if (w->F == MP0) {
+      *state_ptr = w->olim<olim3d_hu_mp0>().get_state_ptr();
+    } else if (w->F == MP1) {
+      *state_ptr = w->olim<olim3d_hu_mp1>().get_state_ptr();
+    } else if (w->F == RHR) {
+      *state_ptr = w->olim<olim3d_hu_rhr>().get_state_ptr();
+    }
+  }
 
   return SUCCESS;
 }
--- src/pyolim.pyx
+++ src/pyolim.pyx
@@ -21,7 +21,6 @@ class Quadrature(Enum):
     RHR = 2
 
 cdef extern from "olim_wrapper.h":
-
     enum neighborhood:
         OLIM4
         OLIM8
@@ -31,6 +30,8 @@ cdef extern from "olim_wrapper.h":
         OLIM3D
 
     enum cost_func:
+        MP0
+        MP1
         RHR
 
     struct olim_wrapper_params:
@@ -63,19 +64,30 @@ cdef class Olim:
     def dims(self):
         if self._p.ndims == 2:
             return self._p.dims[0], self._p.dims[1]
+        elif self._p.ndims == 3:
+            return self._p.dims[0], self._p.dims[1], self._p.dims[2]
 
     @property
     def U(self):
         cdef double * U
+        cdef double[:, ::1] mv_2
+        cdef double[:, :, ::1] mv_3
         olim_wrapper_get_U_ptr(self._w, &U)
-        M, N = self.dims
-        cdef double[:, ::1] mv = <double[:M, :N]> U
-        return np.asarray(mv)
+        if self._p.ndims == 2:
+            M, N = self.dims
+            mv_2 = <double[:M, :N]> U
+            return np.asarray(mv_2)
+        elif self._p.ndims == 3:
+            M, N, P = self.dims
+            mv_3 = <double[:M, :N, :P]> U
+            return np.asarray(mv_3)
 
     @U.setter
     def U(self, U_mv):
         if self._p.ndims == 2:
             self.set_U_2(U_mv)
+        if self._p.ndims == 3:
+            self.set_U_3(U_mv)
 
     cdef set_U_2(self, double[:, ::1] U_mv):
         cdef double * U = NULL
@@ -84,18 +96,34 @@ cdef class Olim:
         cdef double[:, ::1] mv = <double[:M, :N]> U
         mv[...] = U_mv
 
+    cdef set_U_3(self, double[:, :, ::1] U_mv):
+        cdef double * U = NULL
+        olim_wrapper_get_U_ptr(self._w, &U)
+        M, N, P = self.dims
+        cdef double[:, :, ::1] mv = <double[:M, :N, :P]> U
+        mv[...] = U_mv
+
     @property
     def s(self):
         cdef double * s
+        cdef double[:, ::1] mv_2
+        cdef double[:, :, ::1] mv_3
         olim_wrapper_get_s_ptr(self._w, &s)
-        M, N = self.dims
-        cdef double[:, ::1] mv = <double[:M, :N]> s
-        return np.asarray(mv)
+        if self._p.ndims == 2:
+            M, N = self.dims
+            mv_2 = <double[:M, :N]> s
+            return np.asarray(mv_2)
+        elif self._p.ndims == 3:
+            M, N, P = self.dims
+            mv_3 = <double[:M, :N, :P]> s
+            return np.asarray(mv_3)
 
     @s.setter
     def s(self, s_mv):
         if self._p.ndims == 2:
             self.set_s_2(s_mv)
+        if self._p.ndims == 3:
+            self.set_s_3(s_mv)
 
     cdef set_s_2(self, double[:, ::1] s_mv):
         cdef double * s = NULL
@@ -104,26 +132,42 @@ cdef class Olim:
         cdef double[:, ::1] mv = <double[:M, :N]> s
         mv[...] = s_mv
 
+    cdef set_s_3(self, double[:, :, ::1] s_mv):
+        cdef double * s = NULL
+        olim_wrapper_get_s_ptr(self._w, &s)
+        M, N, P = self.dims
+        cdef double[:, :, ::1] mv = <double[:M, :N, :P]> s
+        mv[...] = s_mv
+
     @property
     def state(self):
         cdef char * state
+        cdef char[:, ::1] mv_2
+        cdef char[:, :, ::1] mv_3
         olim_wrapper_get_state_ptr(self._w, &state)
-        M, N = self.dims
-        cdef char[:, ::1] mv = <char[:M, :N]> state
-        return np.asarray(mv)
+        if self._p.ndims == 2:
+            M, N = self.dims
+            mv_2 = <char[:M, :N]> state
+            return np.asarray(mv_2)
+        elif self._p.ndims == 3:
+            M, N, P = self.dims
+            mv_3 = <char[:M, :N, :P]> state
+            return np.asarray(mv_3)
 
-    def __cinit__(self, nb, quad, double[:, ::1] s, double h):
+    def __cinit__(self, nb, quad, s, double h):
         self._p.nb = nb.value
         self._p.F = quad.value
         self._p.h = h
-        self._p.dims = <int *> malloc(2*sizeof(int))
-        self._p.dims[0] = s.shape[0]
-        self._p.dims[1] = s.shape[1]
-        self._p.ndims = 2
+        self._p.dims = <int *> malloc(s.ndim*sizeof(int))
+        for i in range(s.ndim):
+            self._p.dims[i] = s.shape[i]
+        self._p.ndims = s.ndim
+
         err = olim_wrapper_init(&self._w, &self._p)
         if err != SUCCESS:
             raise Exception('error!')
-        # olim_wrapper_set_s_ptr(self._w, &s[0, 0])
+
+        self.s[...] = s
 
     def __dealloc__(self):
         free(self._p.dims)
@@ -140,9 +184,25 @@ cdef class Olim:
                 self.add_bd_2(list(args[:2]))
             elif len(args) == 3:
                 self.add_bd_2(list(args[:2]), U=args[2])
+            else:
+                raise Exception('error!')
+        elif self._p.ndims == 3:
+            if len(args) == 3:
+                self.add_bd_3(list(args[:3]))
+            elif len(args) == 4:
+                self.add_bd_3(list(args[:3]), U=args[3])
+            else:
+                raise Exception('error!')
 
     cdef add_bd_2(self, list inds, double U=0):
         cdef int inds_[2]
         inds_[0] = inds[0]
         inds_[1] = inds[1]
+        olim_wrapper_add_bd(self._w, inds_, U)
+
+    cdef add_bd_3(self, list inds, double U=0):
+        cdef int inds_[3]
+        inds_[0] = inds[0]
+        inds_[1] = inds[1]
+        inds_[2] = inds[2]
         olim_wrapper_add_bd(self._w, inds_, U)

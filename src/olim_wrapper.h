2b585de3e79eb23f7beff59287c802ec9735b30f
Samuel Potter <sfp@Samuels-MacBook-Air.local>
2019-02-25 19:58:17 -0500

    change pointer indentation in C files

--- src/olim_wrapper.h
+++ src/olim_wrapper.h
@@ -27,20 +27,20 @@ typedef struct olim_wrapper_params {
   enum cost_func F;
   double h;
   int ndims;
-  int * dims;
+  int *dims;
 } olim_wrapper_params_s;
   
 typedef struct olim_wrapper olim_wrapper_s;
 
-status_e olim_wrapper_init(olim_wrapper_s ** w_ptr, olim_wrapper_params_s * p);
-status_e olim_wrapper_deinit(olim_wrapper_s ** w_ptr);
-status_e olim_wrapper_run(olim_wrapper_s * w);
-status_e olim_wrapper_add_src(olim_wrapper_s * w, int * inds, double U);
-status_e olim_wrapper_add_bd(olim_wrapper_s * w, int * inds);
-status_e olim_wrapper_get_U_ptr(olim_wrapper_s * w, double ** U_ptr);
-status_e olim_wrapper_get_s_ptr(olim_wrapper_s * w, double ** s_ptr);
-status_e olim_wrapper_get_state_ptr(olim_wrapper_s * w, char ** state_ptr);
+status_e olim_wrapper_init(olim_wrapper_s **w_ptr, olim_wrapper_params_s *p);
+status_e olim_wrapper_deinit(olim_wrapper_s **w_ptr);
+status_e olim_wrapper_run(olim_wrapper_s *w);
+status_e olim_wrapper_add_src(olim_wrapper_s *w, int *inds, double U);
+status_e olim_wrapper_add_bd(olim_wrapper_s *w, int *inds);
 status_e olim_wrapper_set_fac_src(olim_wrapper_s *w, int *inds, fac_src_wrapper_s *fs);
+status_e olim_wrapper_get_U_ptr(olim_wrapper_s *w, double **U_ptr);
+status_e olim_wrapper_get_s_ptr(olim_wrapper_s *w, double **s_ptr);
+status_e olim_wrapper_get_state_ptr(olim_wrapper_s *w, char **state_ptr);
 
 #ifdef __cplusplus
 }

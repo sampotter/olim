55180f4d6270d27d65bc9aa51b3b4f9c22019ae3
Samuel F. Potter <sfp@umiacs.umd.edu>
2019-09-04 12:55:40 -0400

    comment `fac_src_init`

--- src/fac.h
+++ src/fac.h
@@ -22,6 +22,21 @@ extern "C" {
 
   typedef struct fac_src fac_src_s;
 
+  /**
+   * Initialize a factored source:
+   *
+   * - n: the dimension of the problem (n = 2, 3 for now)
+   * - inds: points to n doubles corresponding to the location of the
+   *   source in "index space"
+   * - s: the value of the slowness at the factored source
+   * - padding: a possible padding value to offset `inds` by if the
+   *   underlying marcher uses padding for speed (e.g., in the case of
+   *   the eikonal solver)
+   *
+   * TODO: later, we may want to replace `s` with a void* to user
+   * defined data if a double isn't enough to specify the boundary
+   * conditions at the factored source.
+   */
   status_e fac_src_init(fac_src_s **f_ptr, int n, double const *inds, double s,
                         int const *padding);
 
